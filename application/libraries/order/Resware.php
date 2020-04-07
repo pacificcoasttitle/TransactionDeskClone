230a2e0a80ea5c27d072b6c8562ffead2dc7b9b1
Hgrp8135 <hitesh.p@crestinfosystems.com>
2020-04-07 13:54:23 +0530

    testing purpose

--- application/libraries/order/Resware.php
+++ application/libraries/order/Resware.php
@@ -17,6 +17,7 @@ class Resware
 
    public function make_request($http_method, $endpoint, $body_params='')
    {
+        error_reporting(E_ALL);
         $userdata = $this->CI->session->userdata('user');
         $login =  $userdata['email'];
         if ($userdata['email'] == 'ghernandez@pct.com') {
@@ -24,27 +25,18 @@ class Resware
         } else {
             $password= 'Pacific2';
         }
-        echo $login."---".$password;
         $ch = curl_init(RESWARE_ORDER_API.$endpoint);                                    
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $http_method);                        
         curl_setopt($ch, CURLOPT_POSTFIELDS, $body_params);                   
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-       // curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
-       // curl_setopt($ch, CURLOPT_USERPWD, "$login:$password");
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array( 
-            'Authorization: Basic Z2hlcm5hbmRlekBwY3QuY29tOkFscGhhNjM3Iw==',                              
+        curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
+        curl_setopt($ch, CURLOPT_USERPWD, "$login:$password");
+        curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                
             'Content-Type: application/json',
             'Content-Length: ' . strlen($body_params))                                 
         ); 
         $error_msg = curl_error($ch);
         $result = curl_exec($ch);
-        echo "here345";
-        echo $result;
-        print_r($result);
-        echo $error_msg;
-        print_r($error_msg);
-        echo "here123";
-        exit;
         return $result;
    }
     
