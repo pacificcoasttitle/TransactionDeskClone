e17d7fe29b6918d3ba80c6bca3014e4bb6d6b16d
Hgrp8135 <hitesh.p@crestinfosystems.com>
2020-04-07 14:51:08 +0530

    testing purpose

--- application/libraries/order/Resware.php
+++ application/libraries/order/Resware.php
@@ -29,6 +29,8 @@ class Resware
         curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $http_method);                        
         curl_setopt($ch, CURLOPT_POSTFIELDS, $body_params);                   
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false );
         curl_setopt($ch, CURLOPT_HTTPAUTH, CURLAUTH_BASIC);
         curl_setopt($ch, CURLOPT_USERPWD, "$login:$password");
         curl_setopt($ch, CURLOPT_HTTPHEADER, array(                                
