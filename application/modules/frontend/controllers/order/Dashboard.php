e179a6b75bf7d35dd84fd1a043a925d98963e6d4
Hgrp8135 <hitesh.p@crestinfosystems.com>
2020-04-29 10:11:24 +0530

    solved cpl issue

--- application/modules/frontend/controllers/order/Dashboard.php
+++ application/modules/frontend/controllers/order/Dashboard.php
@@ -1719,7 +1719,7 @@ class Dashboard extends MX_Controller {
 			$vesting = isset($data['Vesting']) && !empty($data['Vesting']) ? $data['Vesting'] : '';
 			$generated_date = isset($data['CommitmentEffectiveDate']) && !empty($data['CommitmentEffectiveDate']) ? date('Y-m-d H:i:s', strtotime($data['CommitmentEffectiveDate'])) : '';
 			$liens = array();
-			$linkedDocuments = $this->order->get_linked_documents($orderDetails['order_id']);
+			//$linkedDocuments = $this->order->get_linked_documents($orderDetails['order_id']);
 			if(isset($data['Liens']) && !empty($data['Liens']))
 			{
 				foreach ($data['Liens'] as $key => $lien) 
@@ -1754,31 +1754,31 @@ class Dashboard extends MX_Controller {
 							$language = str_replace("_RECORDEDDATE_", " ".$recordedDate , $language);
 						}
 						if(strpos($language, '_INSTRUMENTONLY_') !== false) {
-							foreach($linkedDocuments as $linkedDocument) {
-								$href = 'href=\"DocumentID='.$linkedDocument['api_document_id'].'\"';
-								$sync = $linkedDocument['is_sync'];
-								$api_document_id = $linkedDocument['api_document_id'];
-								$order_id = $linkedDocument['order_id'];
-								$document_name = $linkedDocument['document_name'];
-								if(strpos($language, $href) !== false) {
-									$onclick = "onclick=load_doc($sync, $api_document_id, $order_id, '$document_name');";
-									$language = str_replace($href, $onclick, $language);
-								}
-							}
+							// foreach($linkedDocuments as $linkedDocument) {
+							// 	$href = 'href=\"DocumentID='.$linkedDocument['api_document_id'].'\"';
+							// 	$sync = $linkedDocument['is_sync'];
+							// 	$api_document_id = $linkedDocument['api_document_id'];
+							// 	$order_id = $linkedDocument['order_id'];
+							// 	$document_name = $linkedDocument['document_name'];
+							// 	if(strpos($language, $href) !== false) {
+							// 		$onclick = "onclick=load_doc($sync, $api_document_id, $order_id, '$document_name');";
+							// 		$language = str_replace($href, $onclick, $language);
+							// 	}
+							// }
 							$language = str_replace("_INSTRUMENTONLY_", " ".$instrument , $language);
 						}
 						if(strpos($language, '_PARCELID1_') !== false) {
-							foreach($linkedDocuments as $linkedDocument) {
-								$href = 'href=\"DocumentID='.$linkedDocument['api_document_id'].'\"';
-								$sync = $linkedDocument['is_sync'];
-								$api_document_id = $linkedDocument['api_document_id'];
-								$order_id = $linkedDocument['order_id'];
-								$document_name = $linkedDocument['document_name'];
-								if(strpos($language, $href) !== false) {
-									$onclick = "onclick=load_doc($sync, $api_document_id, $order_id, '$document_name');";
-									$language = str_replace($href, $onclick, $language);
-								}
-							}
+							// foreach($linkedDocuments as $linkedDocument) {
+							// 	$href = 'href=\"DocumentID='.$linkedDocument['api_document_id'].'\"';
+							// 	$sync = $linkedDocument['is_sync'];
+							// 	$api_document_id = $linkedDocument['api_document_id'];
+							// 	$order_id = $linkedDocument['order_id'];
+							// 	$document_name = $linkedDocument['document_name'];
+							// 	if(strpos($language, $href) !== false) {
+							// 		$onclick = "onclick=load_doc($sync, $api_document_id, $order_id, '$document_name');";
+							// 		$language = str_replace($href, $onclick, $language);
+							// 	}
+							// }
 							$language = str_replace("_PARCELID1_", " ".$parcelID , $language);
 						}
 						$language = str_replace("\u000b", "", $language);
@@ -1837,31 +1837,31 @@ class Dashboard extends MX_Controller {
 						$language = str_replace("_RECORDEDDATE_", " ".$recordedDate , $language);
 					}
 					if(strpos($language, '_INSTRUMENTONLY_') !== false) {
-						foreach($linkedDocuments as $linkedDocument) {
-							$href = 'href=\"DocumentID='.$linkedDocument['api_document_id'].'\"';
-							$sync = $linkedDocument['is_sync'];
-							$api_document_id = $linkedDocument['api_document_id'];
-							$order_id = $linkedDocument['order_id'];
-							$document_name = $linkedDocument['document_name'];
-							if(strpos($language, $href) !== false) {
-								$onclick = "onclick=load_doc($sync, $api_document_id, $order_id, '$document_name');";
-								$language = str_replace($href, $onclick, $language);
-							}
-						}
+						// foreach($linkedDocuments as $linkedDocument) {
+						// 	$href = 'href=\"DocumentID='.$linkedDocument['api_document_id'].'\"';
+						// 	$sync = $linkedDocument['is_sync'];
+						// 	$api_document_id = $linkedDocument['api_document_id'];
+						// 	$order_id = $linkedDocument['order_id'];
+						// 	$document_name = $linkedDocument['document_name'];
+						// 	if(strpos($language, $href) !== false) {
+						// 		$onclick = "onclick=load_doc($sync, $api_document_id, $order_id, '$document_name');";
+						// 		$language = str_replace($href, $onclick, $language);
+						// 	}
+						// }
 						$language = str_replace("_INSTRUMENTONLY_", " ".$instrument , $language);
 					}
 					if(strpos($language, '_PARCELID1_') !== false) {
-						foreach($linkedDocuments as $linkedDocument) {
-							$href = 'href=\"DocumentID='.$linkedDocument['api_document_id'].'\"';
-							$sync = $linkedDocument['is_sync'];
-							$api_document_id = $linkedDocument['api_document_id'];
-							$order_id = $linkedDocument['order_id'];
-							$document_name = $linkedDocument['document_name'];
-							if(strpos($language, $href) !== false) {
-								$onclick = "onclick=load_doc($sync, $api_document_id, $order_id, '$document_name');";
-								$language = str_replace($href, $onclick, $language);
-							}
-						}
+						// foreach($linkedDocuments as $linkedDocument) {
+						// 	$href = 'href=\"DocumentID='.$linkedDocument['api_document_id'].'\"';
+						// 	$sync = $linkedDocument['is_sync'];
+						// 	$api_document_id = $linkedDocument['api_document_id'];
+						// 	$order_id = $linkedDocument['order_id'];
+						// 	$document_name = $linkedDocument['document_name'];
+						// 	if(strpos($language, $href) !== false) {
+						// 		$onclick = "onclick=load_doc($sync, $api_document_id, $order_id, '$document_name');";
+						// 		$language = str_replace($href, $onclick, $language);
+						// 	}
+						// }
 						$language = str_replace("_PARCELID1_", " ".$parcelID , $language);
 					}
 					$language = str_replace("\u000b", "", $language);
