e010c1724e81465b88d6cea812d52397997d61f8
viratcrest <info@emailboss.io>
2020-05-21 18:13:27 +0530

    Reverted mail function

--- application/modules/frontend/controllers/order/ReviewPrelim.php
+++ application/modules/frontend/controllers/order/ReviewPrelim.php
@@ -355,8 +355,8 @@ class ReviewPrelim extends MX_Controller {
 
 	    	$emailContent['tax'] = isset($email_data['tax']) && !empty($email_data['tax']) ? json_encode($email_data['tax']) : '';
 	    	$emailContent['liens'] = isset($email_data['liens']) && !empty($email_data['liens']) ? json_encode($email_data['liens']) : '';
-
-	    	/*$this->load->library("phpmailer_library");
+	    	
+	    	$this->load->library("phpmailer_library");
 	    	$mail = $this->phpmailer_library->load();
 	    	$mail->isSendmail();
 			$mail->IsHTML(true);
@@ -369,17 +369,16 @@ class ReviewPrelim extends MX_Controller {
 			$prelim_message_body = $this->load->view('emails/prelim.php',$emailContent,TRUE);
 			
 			$mail->Body = $prelim_message_body;
-			$mail->AltBody = "Use an HTML compatible email client";*/
-			$this->load->library('email');
+			$mail->AltBody = "Use an HTML compatible email client";
+			/*$this->load->library('email');
 
 			$this->email->from("ghernandez@pct.com", "Open Order Desk");
 			$this->email->to('hitesh.p@crestinfosystems.com'); 
 			$this->email->subject("The Prelim Hot Sheet");
 			$prelim_message_body = $this->load->view('emails/prelim.php',$emailContent,TRUE);
-			$this->email->message($prelim_message_body);  
-
+			$this->email->message($prelim_message_body);*/
 			$result = array();
-			if($this->email->send())
+			if($mail->Send())
 			{
 				$result['mail_status'] = 'success';
 			}
