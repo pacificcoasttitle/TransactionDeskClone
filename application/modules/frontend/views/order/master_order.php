56c3089da789a7f771bff9cba90feecf5c791662
Hgrp8135 <hitesh.p@crestinfosystems.com>
2020-06-03 17:10:36 +0530

    search APN functionality

--- application/modules/frontend/views/order/master_order.php
+++ application/modules/frontend/views/order/master_order.php
@@ -174,7 +174,7 @@
 										</div>
 
 										<div class="frm-row">
-											<div class="section colm colm3">
+											<div class="section colm">
 												<a class="button btn-primary search-apn search-apn-button"
 													href="javascript:void(0);" id="search-apn-btn">APN Search</a>
 											</div>
