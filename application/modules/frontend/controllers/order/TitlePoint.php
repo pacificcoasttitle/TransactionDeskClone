714bdc89f1da5941c60e3926ed2e0482083921ec
viratcrest <info@emailboss.io>
2020-07-10 21:46:50 +0530

    doc change

--- application/modules/frontend/controllers/order/TitlePoint.php
+++ application/modules/frontend/controllers/order/TitlePoint.php
@@ -185,6 +185,7 @@ class TitlePoint extends MX_Controller {
 			if($responseStatus == 'Success')
 			{
 				$status = isset($result['RequestSummaries']['RequestSummary']['Status']) && !empty($result['RequestSummaries']['RequestSummary']['Status']) ? $result['RequestSummaries']['RequestSummary']['Status'] : '';
+				echo "<pre>"; print_r($status); exit;
 				if($status == 'Complete')
 				{
 					$resultId = isset($result['RequestSummaries']['RequestSummary']['Order']['Services']['Service']['ThumbNails']['ResultThumbNail'][0]['ID']) && !empty($result['RequestSummaries']['RequestSummary']['Order']['Services']['Service']['ThumbNails']['ResultThumbNail'][0]['ID']) ? $result['RequestSummaries']['RequestSummary']['Order']['Services']['Service']['ThumbNails']['ResultThumbNail'][0]['ID'] : ''; 
@@ -230,6 +231,7 @@ class TitlePoint extends MX_Controller {
 			if($responseStatus == 'Success')
 			{
 				$status = isset($result['RequestSummaries']['RequestSummary']['Status']) && !empty($result['RequestSummaries']['RequestSummary']['Status']) ? $result['RequestSummaries']['RequestSummary']['Status'] : '';
+
 				if($status == 'Complete')
 				{
 					$resultId = isset($result['RequestSummaries']['RequestSummary']['Order']['Services']['Service']['ThumbNails']['ResultThumbNail']['ID']) && !empty($result['RequestSummaries']['RequestSummary']['Order']['Services']['Service']['ThumbNails']['ResultThumbNail']['ID']) ? $result['RequestSummaries']['RequestSummary']['Order']['Services']['Service']['ThumbNails']['ResultThumbNail']['ID'] : '';
