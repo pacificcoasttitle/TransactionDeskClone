ac2845f1b222179ac34830776450e7a65f76e5a2
mehulcrest <mehul.b@crestinfosystems.com>
2020-07-24 15:32:55 +0530

    Changes realted to product types

--- application/modules/frontend/models/order/Home_model.php
+++ application/modules/frontend/models/order/Home_model.php
@@ -157,4 +157,50 @@ class Home_model extends CI_Model
         if($query->num_rows() > 0)
             return $query->row_array();
     }
+
+    public function check_product_type($id)
+    {
+
+    }
+
+    public function get_product_types($params = array())
+    {
+        $table = 'pct_order_product_types';
+
+        $this->db->select('*');
+        $this->db->from($table);
+        
+        if(array_key_exists("where", $params)){
+            foreach($params['where'] as $key => $val){
+                $this->db->where($key, $val);
+            }
+        }
+        
+        if(array_key_exists("returnType",$params) && $params['returnType'] == 'count'){
+            $result = $this->db->count_all_results();
+        }else{
+            if(array_key_exists("id", $params)){
+                $this->db->where('id', $params['id']);
+                $query = $this->db->get();
+                $result = $query->row_array();
+            }
+            else
+            {
+                $this->db->order_by('id', 'asc');
+                if(array_key_exists("start",$params) && array_key_exists("limit",$params))
+                {
+                    $this->db->limit($params['limit'],$params['start']);
+                }
+                elseif(!array_key_exists("start",$params) && array_key_exists("limit",$params))
+                {
+                    $this->db->limit($params['limit']);
+                }
+                
+                $query = $this->db->get();
+                $result = ($query->num_rows() > 0)?$query->result_array():FALSE;
+            }
+        }
+        // Return fetched data
+        return $result;
+    }
 }
