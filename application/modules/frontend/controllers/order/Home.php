58d14380b6311da7eb38226c2f8034164ba5d3b0
mehulcrest <mehul.b@crestinfosystems.com>
2020-08-11 12:31:23 +0530

    Removed message

--- application/modules/frontend/controllers/order/Home.php
+++ application/modules/frontend/controllers/order/Home.php
@@ -844,10 +844,7 @@ class Home extends MX_Controller {
     function orderSubmit()
     {
     	$fileId = $this->uri->segment(2);
-    	echo "<pre>data:"; print_r($this->session->userdata('tp_api_id'));
     	$this->session->unset_userdata('tp_api_id');
-    	echo "<pre>data1:"; print_r($this->session->userdata('tp_api_id')); exit;
-    	// $this->session->unset_userdata('tp_api_id');
 		
 		if($fileId)
 		{
