828a28f5f05b942a6f37c36fe3808b317764a973
mehulcrest <mehul.b@crestinfosystems.com>
2020-08-11 17:55:52 +0530

    Logs message changes

--- application/modules/admin/controllers/order/TitlePoint.php
+++ application/modules/admin/controllers/order/TitlePoint.php
@@ -101,7 +101,7 @@ class TitlePoint extends MX_Controller {
                     {
                         $nestedData[] = 'success';
                     }
-                    else if(strtolower($value['lv_file_status']) != 'success')
+                    else if((strtolower($value['lv_file_status']) != 'success') && !empty($value['lv_file_status']))
                     {
                         $nestedData[] = $value['lv_file_status'];
                     }
@@ -251,7 +251,7 @@ class TitlePoint extends MX_Controller {
                     {
                         $nestedData[] = 'success';
                     }
-                    else if(strtolower($value['tax_file_status']) != 'success')
+                    else if((strtolower($value['tax_file_status']) != 'success') && !empty($value['lv_file_status']))
                     {
                         $nestedData[] = $value['tax_file_status'];
                     }
