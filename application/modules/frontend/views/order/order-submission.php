a00a19468aebba96f63bb20663df3cc35bd7f4db
mehulcrest <mehul.b@crestinfosystems.com>
2020-08-22 10:14:42 +0530

    Changed admin email

--- application/modules/frontend/views/order/order-submission.php
+++ application/modules/frontend/views/order/order-submission.php
@@ -180,6 +180,7 @@
                                     $fips = isset($tp_data['fips']) && !empty($tp_data['fips']) ? $tp_data['fips'] : '';
                                 ?>
                                 <?php
+                                $lv_file_url='';
                                     if(isset($lv_file_url) && !empty($lv_file_url))
                                     {
                                 ?>
@@ -207,6 +208,7 @@
                             <div class="col-md-3">
                                 <div id="instrumentInfoFile">
                                     <?php
+                                        $deed_file_url = '';
                                         if(isset($deed_file_url) && !empty($deed_file_url))
                                         {
                                     ?>
@@ -260,6 +262,7 @@
                             <div class="col-md-3">
                                 <div id="taxDocumentInfo">
                                     <?php
+                                    $tax_file_url = '';
                                         if(isset($tax_file_url) && !empty($tax_file_url))
                                         {
                                     ?>
