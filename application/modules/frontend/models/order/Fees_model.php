d05ea587a352eb7c9be7972703a76dab23c583d3
mehulcrest <mehul.b@crestinfosystems.com>
2020-08-27 17:50:29 +0530

    Added fees type condition in frontend

--- application/modules/frontend/models/order/Fees_model.php
+++ application/modules/frontend/models/order/Fees_model.php
@@ -122,7 +122,7 @@ class Fees_model extends CI_Model
     {
         $table = $this->table;
 
-        $this->db->select('*');
+        $this->db->select('pct_order_fees.*, pct_order_fees_types.name as fee_type');
         $this->db->from($table);
         
         if(array_key_exists("where", $params)){
@@ -131,21 +131,26 @@ class Fees_model extends CI_Model
             }
         }
         
-        if(array_key_exists("returnType",$params) && $params['returnType'] == 'count'){
+        if(array_key_exists("returnType",$params) && $params['returnType'] == 'count')
+        {
+            $this->db->join('pct_order_fees_types','pct_order_fees.fee_type_id = pct_order_fees_types.id');
             $result = $this->db->count_all_results();
-        }else{
+        }
+        else
+        {
             if(array_key_exists("id", $params)){
                 $this->db->where('id', $params['id']);
+                $this->db->join('pct_order_fees_types','pct_order_fees.fee_type_id = pct_order_fees_types.id');
                 $query = $this->db->get();
                 $result = $query->row_array();
             }else{
-                $this->db->order_by('id', 'asc');
+                $this->db->order_by('pct_order_fees.id', 'desc');
                 if(array_key_exists("start",$params) && array_key_exists("limit",$params)){
                     $this->db->limit($params['limit'],$params['start']);
                 }elseif(!array_key_exists("start",$params) && array_key_exists("limit",$params)){
                     $this->db->limit($params['limit']);
                 }
-                
+                $this->db->join('pct_order_fees_types','pct_order_fees.fee_type_id = pct_order_fees_types.id');
                 $query = $this->db->get();
                 $result = ($query->num_rows() > 0)?$query->result_array():FALSE;
             }
@@ -154,5 +159,4 @@ class Fees_model extends CI_Model
         // Return fetched data
         return $result;
     }
-
 }
