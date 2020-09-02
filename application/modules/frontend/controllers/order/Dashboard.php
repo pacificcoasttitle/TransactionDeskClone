8ff23ebf392d37c195715f8a2e0df586532179d7
Hgrp8135 <hitesh.p@crestinfosystems.com>
2020-09-02 11:24:42 +0530

    prelim changes

--- application/modules/frontend/controllers/order/Dashboard.php
+++ application/modules/frontend/controllers/order/Dashboard.php
@@ -29,6 +29,7 @@ class Dashboard extends MX_Controller {
 		$is_master = isset($userdata['is_master']) && !empty($userdata['is_master']) ? $userdata['is_master'] : '';
 		$data['name'] = $name;
 		$data['is_master'] = $is_master;
+		$data['user_email'] = $userdata['email'];
 		$data['is_special_lender'] = $userdata['is_special_lender'] == 1 ? 1 : 0;
 		$data['order_lists'] = $this->order->get_recent_orders();
 		$data['title'] = 'Smart Dashboard | Pacific Coast Title Company';
--- application/modules/frontend/views/order/dashboard.php
+++ application/modules/frontend/views/order/dashboard.php
@@ -50,9 +50,11 @@
 
 								?>
 								
-								 <a href="<?php echo base_url().'prelim-files'; ?>">
-									<button class="btn1 btn-type-1c btn-lg" type="button">Review Prelim</button>
-								</a>
+								<?php if (isset($email) && empty($email) && $email == 'teamrestine@eatonescrow.com') { ?>
+									<a href="<?php echo base_url().'prelim-files'; ?>">
+										<button class="btn1 btn-type-1c btn-lg" type="button">Review Prelim</button>
+									</a>
+								<?php } ?>
                   				<!--<a href="<?php // echo base_url().'attach-files'; ?>">
                   					<button class="btn1 btn-type-1d btn-lg" type="button">Upload Doc</button>
                   				</a> -->
