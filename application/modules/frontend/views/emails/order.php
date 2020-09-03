d35570fa53a4de638d66108c4f83f2a9543eb3a2
mehulcrest <mehul.b@crestinfosystems.com>
2020-09-03 14:16:36 +0530

    Link added

--- application/modules/frontend/views/emails/order.php
+++ application/modules/frontend/views/emails/order.php
@@ -416,7 +416,7 @@
                                             <tbody>
                                                 <tr>
                                                     <td align="center" valign="middle" class="mcnButtonContent" style="font-family: Helvetica; font-size: 18px; padding: 18px;">
-                                                        <a class="mcnButton " title="Generate Fees" href="<?php // echo base_url().'generate-fees/'. $orderId; ?>" target="_blank" style="font-weight: bold;letter-spacing: -0.5px;line-height: 100%;text-align: center;text-decoration: none;color: #FFFFFF;">Generate Fees</a>
+                                                        <a class="mcnButton " title="Generate Fees" href="<?php echo base_url().'generate-fees/'. $orderId; ?>" target="_blank" style="font-weight: bold;letter-spacing: -0.5px;line-height: 100%;text-align: center;text-decoration: none;color: #FFFFFF;">Generate Fees</a>
                                                     </td>
                                                 </tr>
                                             </tbody>
