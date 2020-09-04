613cb5459a85b5eb37cd8e3db683bf0e67571fd3
Hgrp8135 <hitesh.p@crestinfosystems.com>
2020-09-04 11:41:05 +0530

    linked doc issue

--- application/modules/frontend/controllers/order/Dashboard.php
+++ application/modules/frontend/controllers/order/Dashboard.php
@@ -1939,7 +1939,7 @@ class Dashboard extends MX_Controller {
 								$pages = array(4, 5, 6, 7, 8, 9);
 								$linkedDocCount = 0;
 								foreach ($pages as $page) {
-									/*$html = $pdf->html($page);
+									$html = $pdf->html($page);
 									$total_pages = $pdf->getPages();
 									$htmlDom = new DOMDocument;
 									@$htmlDom->loadHTML($html);
@@ -1999,7 +1999,7 @@ class Dashboard extends MX_Controller {
 										} else{
 											continue;
 										}
-									}*/
+									}
 								}
 							}	
 						} else {
@@ -2094,7 +2094,7 @@ class Dashboard extends MX_Controller {
 								$pages = array(4, 5, 6, 7, 8, 9);
 								$linkedDocCount = 0;
 								foreach ($pages as $page) {
-									/*$html = $pdf->html($page);
+									$html = $pdf->html($page);
 									$total_pages = $pdf->getPages();
 									$htmlDom = new DOMDocument();
 									@$htmlDom->loadHTML($html);
@@ -2159,7 +2159,7 @@ class Dashboard extends MX_Controller {
 												continue;
 											}
 										}
-									}*/
+									}
 									
 								}
 							}
