5738786ccf714d5388f8bb7a64d8e9ce2dd28b2d
hiteshcrest <hitesh.p@crestinfosystems.com>
2020-09-18 23:19:41 +0530

    cpl changes

--- application/modules/frontend/controllers/order/Dashboard.php
+++ application/modules/frontend/controllers/order/Dashboard.php
@@ -3350,7 +3350,7 @@ class Dashboard extends MX_Controller {
 				$orderDetails['lender_address'] = $lenderDetails['street_address'] ? $lenderDetails['street_address'] : '';
 				$orderDetails['lender_city'] = $lenderDetails['city'] ? $lenderDetails['city'] : '';
 				$orderDetails['lender_zipcode'] = $lenderDetails['zip_code'] ? $lenderDetails['zip_code'] : '';
-				$orderDetails['lender_assignment_clause'] = $orderDetails['assignment_clause'] ? $orderDetails['assignment_clause'] : '';
+				$orderDetails['lender_assignment_clause'] = $lenderDetails['assignment_clause'] ? $lenderDetails['assignment_clause'] : '';
 				$orderDetails['lender_id'] = $lenderDetails['id'] ? $lenderDetails['id'] : '';
 			} else {
 				$orderDetails['lender_first_name'] =  '';
