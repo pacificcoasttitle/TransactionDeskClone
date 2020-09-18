b33011bd6fb31fd400732ce8628dc5c8ea6f948e
hiteshcrest <hitesh.p@crestinfosystems.com>
2020-09-18 18:14:42 +0530

    cpl changes

--- application/libraries/order/Natic.php
+++ application/libraries/order/Natic.php
@@ -116,7 +116,6 @@ class Natic
                     <Type>String</Type>
                     <Required>true</Required>
                 </Field>
-                
                 <Field>
                     <FieldId>LenderContactName</FieldId>
                     <Name>Lender Contact Name</Name>
