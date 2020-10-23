2e6f031e84dd462f7c7447eff6a91c5b0930ee65
hiteshcrest <hitesh.p@crestinfosystems.com>
2020-10-23 17:16:56 +0530

    login changes

--- application/modules/admin/models/order/Home_model.php
+++ application/modules/admin/models/order/Home_model.php
@@ -24,6 +24,24 @@ class Home_model extends CI_Model
         }
     }
 
+    public function get_user($params = array()) 
+    {
+        $table = $this->table;
+        $this->db->select('*');
+        $this->db->from($table);
+        foreach($params as $key => $val){
+            $this->db->where($key, $val);
+        }
+        $query = $this->db->get();
+        $result = $query->row_array();
+        if(!empty($result)) { 
+            return $result;
+        } else {
+            return array();
+        }
+        
+    }
+
     public function get_customers($params)
     {
         $is_escrow = isset($params['is_escrow']) && !empty($params['is_escrow']) ? $params['is_escrow'] : 0;
