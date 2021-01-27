bda23f2a8f9600edad5a9eddc8cb64487973751a
hiteshcrest <hitesh.p@crestinfosystems.com>
2021-01-27 15:11:36 +0530

    on hold changes

--- application/modules/frontend/controllers/order/Cron.php
+++ application/modules/frontend/controllers/order/Cron.php
@@ -2212,7 +2212,7 @@ class Cron extends MX_Controller {
                     $from_mail = env('FROM_EMAIL');
                     $subject = 'Notification For On Hold Order';
                     //$to = 'cs@pct.com';
-                    $to = 'hitesh.p2crestinfosystems.com';
+                    $to = 'hitesh.p@crestinfosystems.com';
                     $this->load->helper('sendemail');
                     
                     $mail_result = send_email($from_mail,$from_name, $to, $subject, $message);
