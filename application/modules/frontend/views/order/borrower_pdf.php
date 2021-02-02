e81b06dee87be44232d01343c68539adfcb1190c
mehulcrest <mehul.b@crestinfosystems.com>
2021-02-02 15:14:44 +0530

    Changes related to pdf view

--- application/modules/frontend/views/order/borrower_pdf.php
+++ application/modules/frontend/views/order/borrower_pdf.php
@@ -31,22 +31,25 @@
     <table class="table table-bordered">
         <thead>
             <tr>
-                <th scope="col">Home Phone</th>
+                <!-- <th scope="col">Home Phone</th> -->
                 <th scope="col">Mobile Phone</th>
                 <th scope="col">Date of Birth</th>
+                <th scope="col">SSN Last 4 Digits</th>
             </tr>
         </thead>
         <tbody>
             <tr>
-                <td><?php echo isset($borrower_info[0]['telephone']) && !empty($borrower_info[0]['telephone']) ? $borrower_info[0]['telephone'] : ''; ?></td>
+                <!-- <td><?php // echo isset($borrower_info[0]['telephone']) && !empty($borrower_info[0]['telephone']) ? $borrower_info[0]['telephone'] : ''; ?></td> -->
                 <td><?php echo isset($borrower_info[0]['mobile']) && !empty($borrower_info[0]['mobile']) ? $borrower_info[0]['mobile'] : ''; ?></td>
 
                 <td><?php echo isset($borrower_info[0]['date_of_birth']) && !empty($borrower_info[0]['date_of_birth']) ? date("m/d/Y",strtotime($borrower_info[0]['date_of_birth'])) : ''; ?></td>
+
+                <td><?php echo isset($borrower_info[0]['ssn']) && !empty($borrower_info[0]['ssn']) ? $borrower_info[0]['ssn'] : ''; ?></td>
             </tr>
         </tbody>
     </table>
 
-    <table class="table table-bordered">
+    <!-- <table class="table table-bordered">
         <thead>
             <tr>
                 <th scope="col">Birthplace</th>
@@ -56,12 +59,12 @@
         </thead>
         <tbody>
             <tr>
-                <td><?php echo isset($borrower_info[0]['birthplace']) && !empty($borrower_info[0]['birthplace']) ? $borrower_info[0]['birthplace'] : ''; ?></td>
-                <td><?php echo isset($borrower_info[0]['ssn']) && !empty($borrower_info[0]['ssn']) ? $borrower_info[0]['ssn'] : ''; ?></td>
-                <td><?php echo isset($borrower_info[0]['dln']) && !empty($borrower_info[0]['dln']) ? $borrower_info[0]['dln'] : ''; ?></td>
+                <td><?php // echo isset($borrower_info[0]['birthplace']) && !empty($borrower_info[0]['birthplace']) ? $borrower_info[0]['birthplace'] : ''; ?></td>
+                <td><?php // echo isset($borrower_info[0]['ssn']) && !empty($borrower_info[0]['ssn']) ? $borrower_info[0]['ssn'] : ''; ?></td>
+                <td><?php // echo isset($borrower_info[0]['dln']) && !empty($borrower_info[0]['dln']) ? $borrower_info[0]['dln'] : ''; ?></td>
             </tr>
         </tbody>
-    </table>
+    </table> -->
     <?php 
         /*if($borrower_info[0]['status'] == 'single')
         {*/
@@ -100,21 +103,23 @@
             <table class="table table-bordered">
                 <thead>
                     <tr>
-                        <th scope="col">Spouse Home Phone</th>
+                        <!-- <th scope="col">Spouse Home Phone</th> -->
                         <th scope="col">Spouse Mobile Phone</th>
-                        <th scope="col">Spouse Date of Birth</th>
+                        <!-- <th scope="col">Spouse Date of Birth</th> -->
+                        <th scope="col">Spouse SSN Last 4 Digits</th>
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td><?php echo isset($borrower_info[0]['spouse_telephone']) && !empty($borrower_info[0]['spouse_telephone']) ? $borrower_info[0]['spouse_telephone'] : ''; ?></td>
+                        <!-- <td><?php // echo isset($borrower_info[0]['spouse_telephone']) && !empty($borrower_info[0]['spouse_telephone']) ? $borrower_info[0]['spouse_telephone'] : ''; ?></td> -->
                         <td><?php echo isset($borrower_info[0]['spouse_mobile']) && !empty($borrower_info[0]['spouse_mobile']) ? $borrower_info[0]['spouse_mobile'] : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['spouse_date_of_birth']) && !empty($borrower_info[0]['spouse_date_of_birth']) ? date("m/d/Y",strtotime($borrower_info[0]['spouse_date_of_birth'])) : ''; ?></td>
+                        <!-- <td><?php // echo isset($borrower_info[0]['spouse_date_of_birth']) && !empty($borrower_info[0]['spouse_date_of_birth']) ? date("m/d/Y",strtotime($borrower_info[0]['spouse_date_of_birth'])) : ''; ?></td> -->
+                        <td><?php echo isset($borrower_info[0]['spouse_ssn']) && !empty($borrower_info[0]['spouse_ssn']) ? $borrower_info[0]['spouse_ssn'] : ''; ?></td>
                     </tr>
                 </tbody>
             </table>
 
-            <table class="table table-bordered">
+            <!-- <table class="table table-bordered">
                 <thead>
                     <tr>
                         <th scope="col">Spouse Birthplace</th>
@@ -124,12 +129,12 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td><?php echo isset($borrower_info[0]['spouse_birthplace']) && !empty($borrower_info[0]['spouse_birthplace']) ? $borrower_info[0]['spouse_birthplace'] : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['spouse_ssn']) && !empty($borrower_info[0]['spouse_ssn']) ? $borrower_info[0]['spouse_ssn'] : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['spouse_dln']) && !empty($borrower_info[0]['spouse_dln']) ? $borrower_info[0]['spouse_dln'] : ''; ?></td>
+                        <td><?php // echo isset($borrower_info[0]['spouse_birthplace']) && !empty($borrower_info[0]['spouse_birthplace']) ? $borrower_info[0]['spouse_birthplace'] : ''; ?></td>
+                        <td><?php // echo isset($borrower_info[0]['spouse_ssn']) && !empty($borrower_info[0]['spouse_ssn']) ? $borrower_info[0]['spouse_ssn'] : ''; ?></td>
+                        <td><?php // echo isset($borrower_info[0]['spouse_dln']) && !empty($borrower_info[0]['spouse_dln']) ? $borrower_info[0]['spouse_dln'] : ''; ?></td>
                     </tr>
                 </tbody>
-            </table>
+            </table> -->
     <?php
         }
     ?>
@@ -157,21 +162,23 @@
             <table class="table table-bordered">
                 <thead>
                     <tr>
-                        <th scope="col">Partner Home Phone</th>
+                        <!-- <th scope="col">Partner Home Phone</th> -->
                         <th scope="col">Partner Mobile Phone</th>
-                        <th scope="col">Partner Date of Birth</th>
+                        <th scope="col">Partner SSN Last 4</th>
+                        <!-- <th scope="col">Partner Date of Birth</th> -->
                     </tr>
                 </thead>
                 <tbody>
                     <tr>
-                        <td><?php echo isset($borrower_info[0]['partner_telephone']) && !empty($borrower_info[0]['partner_telephone']) ? $borrower_info[0]['partner_telephone'] : ''; ?></td>
+                        <!-- <td><?php // echo isset($borrower_info[0]['partner_telephone']) && !empty($borrower_info[0]['partner_telephone']) ? $borrower_info[0]['partner_telephone'] : ''; ?></td> -->
                         <td><?php echo isset($borrower_info[0]['partner_mobile']) && !empty($borrower_info[0]['partner_mobile']) ? $borrower_info[0]['partner_mobile'] : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['partner_date_of_birth']) && !empty($borrower_info[0]['partner_date_of_birth']) ? date("m/d/Y",strtotime($borrower_info[0]['partner_date_of_birth'])) : ''; ?></td>
+                        <td><?php echo isset($borrower_info[0]['partner_ssn']) && !empty($borrower_info[0]['partner_ssn']) ? $borrower_info[0]['partner_ssn'] : ''; ?></td>
+                        <!-- <td><?php // echo isset($borrower_info[0]['partner_date_of_birth']) && !empty($borrower_info[0]['partner_date_of_birth']) ? date("m/d/Y",strtotime($borrower_info[0]['partner_date_of_birth'])) : ''; ?></td> -->
                     </tr>
                 </tbody>
             </table>
 
-            <table class="table table-bordered">
+            <!-- <table class="table table-bordered">
                 <thead>
                     <tr>
                         <th scope="col">Partner Birthplace</th>
@@ -181,12 +188,12 @@
                 </thead>
                 <tbody>
                     <tr>
-                        <td><?php echo isset($borrower_info[0]['partner_birthplace']) && !empty($borrower_info[0]['partner_birthplace']) ? $borrower_info[0]['partner_birthplace'] : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['partner_ssn']) && !empty($borrower_info[0]['partner_ssn']) ? $borrower_info[0]['partner_ssn'] : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['partner_dln']) && !empty($borrower_info[0]['partner_dln']) ? $borrower_info[0]['partner_dln'] : ''; ?></td>
+                        <td><?php // echo isset($borrower_info[0]['partner_birthplace']) && !empty($borrower_info[0]['partner_birthplace']) ? $borrower_info[0]['partner_birthplace'] : ''; ?></td>
+                        <td><?php // echo isset($borrower_info[0]['partner_ssn']) && !empty($borrower_info[0]['partner_ssn']) ? $borrower_info[0]['partner_ssn'] : ''; ?></td>
+                        <td><?php // echo isset($borrower_info[0]['partner_dln']) && !empty($borrower_info[0]['partner_dln']) ? $borrower_info[0]['partner_dln'] : ''; ?></td>
                     </tr>
                 </tbody>
-            </table>
+            </table> -->
     <?php
         }
     ?>
@@ -221,11 +228,36 @@
     <?php
         }
     ?>
+    <h2>3. About the Transaction</h2>
+    <table class="table table-bordered">
+        <thead>
+            <tr>
+                <th scope="col">Street Address</th>
+                <th scope="col">Buyer Intends to reside</th>
+                <th scope="col">The land is improved</th>
+                <th scope="col">Property a SFR, 1-4 Units, or Condominium</th>
+                <th scope="col">Work done on the premise on the last 6 months</th>
+                <th scope="col">Previously married</th>
+                
+            </tr>
+        </thead>
+        <tbody>                    
+            <tr>
+                <td><?php echo isset($borrower_info[0]['street_address']) && !empty($borrower_info[0]['street_address']) ? 'Yes' : 'No'; ?></td>
+                <td><?php echo isset($borrower_info[0]['buyer_intends_to_reside']) && !empty($borrower_info[0]['buyer_intends_to_reside']) ? 'Yes' : 'No'; ?></td>
+                <td><?php echo isset($borrower_info[0]['land_is_unimproved']) && !empty($borrower_info[0]['land_is_unimproved']) ? 'Yes' : 'No'; ?></td>
+                <td><?php echo isset($borrower_info[0]['type_of_property']) && !empty($borrower_info[0]['type_of_property']) ? 'Yes' : 'No'; ?></td>
+                <td><?php echo isset($borrower_info[0]['work_done_last_6_month']) && !empty($borrower_info[0]['work_done_last_6_month']) ? 'Yes' : 'No'; ?></td>
+                <td><?php echo isset($borrower_info[0]['previously_married']) && !empty($borrower_info[0]['previously_married']) ? 'Yes' : 'No'; ?></td>
+                
+            </tr>
+        </tbody>
+    </table>
     <?php 
         if(isset($borrower_employment_info) && !empty($borrower_employment_info))
         {
     ?>
-            <h2>3. Employment</h2>
+            <h2>4. Employment</h2>
             <table class="table table-bordered">
                 <thead>
                     <tr>
@@ -233,7 +265,7 @@
                         <th scope="col">Address</th>
                         <th scope="col">From</th>
                         <th scope="col">To</th>
-                        <th scope="col">Partner Occupation</th>
+                        <!-- <th scope="col">Partner Occupation</th> -->
                     </tr>
                 </thead>
                 <tbody>
@@ -246,7 +278,7 @@
                                 <td><?php echo isset($v['address']) && !empty($v['address']) ? $v['address'] : ''; ?></td>
                                 <td><?php echo isset($v['from_date']) && !empty($v['from_date']) ? $v['from_date'] : ''; ?></td>
                                 <td><?php echo isset($v['to_date']) && !empty($v['to_date']) ? $v['to_date'] : ''; ?></td>
-                                <td><?php echo isset($v['is_partner_info']) && !empty($v['is_partner_info']) ? 'Yes' : 'No'; ?></td>
+                                <!-- <td><?php // echo isset($v['is_partner_info']) && !empty($v['is_partner_info']) ? 'Yes' : 'No'; ?></td> -->
                             </tr>                            
                     <?php
                         }
@@ -255,137 +287,6 @@
             </table>
     <?php
         }
-    ?>
-    <?php
-        if(isset($borrower_info[0]['partnership_status']) && !empty($borrower_info[0]['partnership_status']))
-        {
-    ?>
-            <h2>4. Partnerships</h2>
-            <table class="table table-bordered">
-                <tr>
-                    <th scope="col">Partnership Status</th>
-                    <td><?php echo ucfirst($borrower_info[0]['partnership_status']); ?></td>
-                </tr>
-            </table>    
-    <?php
-        } 
-    ?>
-    
-    <?php 
-        if(isset($borrower_info[0]['partnership_status']) && !empty($borrower_info[0]['partnership_status']) && $borrower_info[0]['partnership_status'] == 'me')
-        {
-    ?>
-            
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col">Prior Spouse Name</th>
-                        <th scope="col">Reason For End</th>
-                        <th scope="col">End</th>
-                    </tr>
-                </thead>
-                <tbody>                    
-                    <tr>
-                        <td><?php echo isset($borrower_info[0]['prior_spouse_name']) && !empty($borrower_info[0]['prior_spouse_name']) ? $borrower_info[0]['prior_spouse_name'] : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['prior_spouse_reason']) && !empty($borrower_info[0]['prior_spouse_reason']) ? $borrower_info[0]['prior_spouse_reason'] : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['prior_spouse_end']) && !empty($borrower_info[0]['prior_spouse_end']) ? $borrower_info[0]['prior_spouse_end'] : ''; ?></td>
-                    </tr>
-                </tbody>
-            </table>
-    <?php
-        }
-    ?>
-
-    <?php 
-        if(isset($borrower_info[0]['partnership_status']) && !empty($borrower_info[0]['partnership_status']) && $borrower_info[0]['partnership_status'] == 'spouseonly')
-        {
-    ?>
-            
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col">Current Spouse's Prior Spouse Name</th>
-                        <th scope="col">Reason For End</th>
-                        <th scope="col">End</th>
-                    </tr>
-                </thead>
-                <tbody>                    
-                    <tr>
-                        <td><?php echo isset($borrower_info[0]['current_spouse_prior_spouse_name']) && !empty($borrower_info[0]['current_spouse_prior_spouse_name']) ? $borrower_info[0]['current_spouse_prior_spouse_name'] : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['current_spouse_prior_spouse_reason']) && !empty($borrower_info[0]['current_spouse_prior_spouse_reason']) ? $borrower_info[0]['current_spouse_prior_spouse_reason'] : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['current_spouse_prior_spouse_end']) && !empty($borrower_info[0]['current_spouse_prior_spouse_end']) ? $borrower_info[0]['current_spouse_prior_spouse_end'] : ''; ?></td>
-                    </tr>
-                </tbody>
-            </table>
-    <?php
-        }
-    ?>
-
-    <?php 
-        if(isset($borrower_info[0]['partnership_status']) && !empty($borrower_info[0]['partnership_status']) && $borrower_info[0]['partnership_status'] == 'both')
-        {
-    ?>
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">Prior Spouse Name</th>
-                    <th scope="col">Reason For End</th>
-                    <th scope="col">End</th>
-                </tr>
-            </thead>
-            <tbody>                    
-                <tr>
-                    <td><?php echo isset($borrower_info[0]['prior_spouse_name']) && !empty($borrower_info[0]['prior_spouse_name']) ? $borrower_info[0]['prior_spouse_name'] : ''; ?></td>
-                    <td><?php echo isset($borrower_info[0]['prior_spouse_reason']) && !empty($borrower_info[0]['prior_spouse_reason']) ? $borrower_info[0]['prior_spouse_reason'] : ''; ?></td>
-                    <td><?php echo isset($borrower_info[0]['prior_spouse_end']) && !empty($borrower_info[0]['prior_spouse_end']) ? $borrower_info[0]['prior_spouse_end'] : ''; ?></td>
-                </tr>
-            </tbody>
-        </table>            
-        <table class="table table-bordered">
-            <thead>
-                <tr>
-                    <th scope="col">Current Spouse's Prior Spouse Name</th>
-                    <th scope="col">Reason For End</th>
-                    <th scope="col">End</th>
-                </tr>
-            </thead>
-            <tbody>                    
-                <tr>
-                    <td><?php echo isset($borrower_info[0]['current_spouse_prior_spouse_name']) && !empty($borrower_info[0]['current_spouse_prior_spouse_name']) ? $borrower_info[0]['current_spouse_prior_spouse_name'] : ''; ?></td>
-                    <td><?php echo isset($borrower_info[0]['current_spouse_prior_spouse_reason']) && !empty($borrower_info[0]['current_spouse_prior_spouse_reason']) ? $borrower_info[0]['current_spouse_prior_spouse_reason'] : ''; ?></td>
-                    <td><?php echo isset($borrower_info[0]['current_spouse_prior_spouse_end']) && !empty($borrower_info[0]['current_spouse_prior_spouse_end']) ? $borrower_info[0]['current_spouse_prior_spouse_end'] : ''; ?></td>
-                </tr>
-            </tbody>
-        </table>
-    <?php
-        }
-    ?>
-
-    <?php
-        if(isset($borrower_info[0]['street_address']) && !empty($borrower_info[0]['street_address']))
-        {
-    ?>
-            <h2>5. About The Property</h2>
-            <table class="table table-bordered">
-                <thead>
-                    <tr>
-                        <th scope="col">Street Address</th>
-                        <th scope="col">The land is unimproved</th>
-                        <th scope="col">Type of property on land</th>
-                        <th scope="col">Buyer Intends to reside</th>
-                    </tr>
-                </thead>
-                <tbody>                    
-                    <tr>
-                        <td><?php echo isset($borrower_info[0]['street_address']) && !empty($borrower_info[0]['street_address']) ? $borrower_info[0]['street_address'] : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['land_is_unimproved']) && !empty($borrower_info[0]['land_is_unimproved']) ? ucfirst($borrower_info[0]['land_is_unimproved']) : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['type_of_property']) && !empty($borrower_info[0]['type_of_property']) ? ucfirst(str_replace('_', ' ', $borrower_info[0]['type_of_property'])) : ''; ?></td>
-                        <td><?php echo isset($borrower_info[0]['buyer_intends_to_reside']) && !empty($borrower_info[0]['buyer_intends_to_reside']) ? ucfirst($borrower_info[0]['buyer_intends_to_reside']) : ''; ?></td>
-                    </tr>
-                </tbody>
-            </table>   
-    <?php
-        } 
     ?>
 </body>
 </html>
