869d3d2d6a6401ca22bca36d25835b5524818fa2
hiteshcrest <hitesh.p@crestinfosystems.com>
2021-02-18 11:11:40 +0530

    escrow officer changes

--- application/modules/frontend/controllers/order/Home.php
+++ application/modules/frontend/controllers/order/Home.php
@@ -967,6 +967,7 @@ class Home extends MX_Controller {
 								);
 								$escrowCompanyData = $this->home_model->get_company_rows($con);
 								$escrow_email = $escrowCompanyData[0]['email'];
+								$escrow_email = 'hitesh.p@crestinfosystems.com';
 							}
 
 							if (!empty($escrow_email)) {							
