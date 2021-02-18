96aa9647e412c44e8201dde557adaf6c4abec39b
hiteshcrest <hitesh.p@crestinfosystems.com>
2021-02-18 11:47:13 +0530

    escrow officer changes

--- application/modules/frontend/controllers/order/DashboardMail.php
+++ application/modules/frontend/controllers/order/DashboardMail.php
@@ -3287,13 +3287,14 @@ class DashboardMail extends MX_Controller {
         $error_msg = curl_error($ch);
         $result = curl_exec($ch);
         $this->apiLogs->syncLogs(0, 'safewire', 'create_order_on_safewire', $url, $body_params, $result, $order_id, $logid);
-       
-        $result = json_decode(curl_exec($ch), true);
-        if(!empty($result['action_link'])) {
+        $res = json_decode(curl_exec($result), true);
+        if(isset($res['action_link']) && !empty($res['action_link'])) {
             $this->home_model->update(array('is_create_order_on_safewire' => 1), array('id' => $orderDetails['order_id']), 'order_details');
-            $response = array('success' => true, 'message'=> 'order created successfully', 'action_link' => $result['action_link']);
+            $response = array('success' => true, 'message'=> 'order created successfully', 'action_link' => $res['action_link']);
+        } else if(isset($res['result']) && $res['result'] == 'success') {
+            $response = array('success' => true, 'message'=> 'order updated successfully', 'action_link' => $res['action_link']);
         } else {
-            $response = array('success' => false, 'message'=> $result['error']);
+            $response = array('success' => false, 'message'=> $res['error']);
         }
         echo json_encode($response); exit;
     }
