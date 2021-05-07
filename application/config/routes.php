7a13a44c63ff406df45e3fdee3d2906f508ab4df
hiteshcrest <hitesh.p@crestinfosystems.com>
2021-05-07 14:56:11 +0530

    duplicate changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -271,6 +271,7 @@ $route['order/admin/safewire-orders'] = 'admin/order/order/safewireOrders';
 $route['order/admin/get-safewire-orders-list'] = 'admin/order/order/get_safewire_orders_list';
 $route['store-deliverables'] = 'admin/order/home/storeDeliverables';
 $route['download-aws-document-admin'] = 'admin/order/home/downloadAwsDocument';
+$route['update-avoid-duplication-flag'] = 'admin/order/home/updateAvoidDuplicationFlag';
 
 
 
