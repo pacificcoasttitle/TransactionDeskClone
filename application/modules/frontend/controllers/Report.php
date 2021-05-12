5f3b620d4c51e16430c8e1d35ea59436119deeca
Binita Desai <binita.s@crestinfosystems.net>
2021-05-12 10:30:01 +0530

    check issue

--- application/modules/frontend/controllers/Report.php
+++ application/modules/frontend/controllers/Report.php
@@ -278,7 +278,7 @@ class Report extends MX_Controller {
 
 
         $html = $this->load->view('report/report_pdf',$data,true);
-        $html = '<h1>Test</h1>';
+        // $html = '<h1>Test</h1>';
         
         $this->load->library('snappy_pdf');
         
