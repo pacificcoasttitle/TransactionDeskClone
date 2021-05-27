9580018818d5de4391a08dc95f193c68d715d182
hiteshcrest <hitesh.p@crestinfosystems.com>
2021-05-27 16:27:38 +0530

    send email to escrow user changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -198,6 +198,7 @@ $route['transfer-all-files-on-aws'] = 'frontend/order/cron/transferAllFilesOnAws
 $route['download-aws-document'] = 'frontend/order/home/downloadAwsDocument';
 $route['download-aws-document-mail'] = 'frontend/order/dashboardMail/downloadAwsDocument';
 $route['export-data-from-xml-file'] = 'frontend/order/cron/exportDataFromXmlFile';
+$route['send-borrower-verification-mail'] = 'frontend/order/cron/sendMailEscrowUsersForBorrowerVerification';
 
 /* Route for PCT-Order backend*/
 $route['order/admin'] = 'admin/order/home/login';
