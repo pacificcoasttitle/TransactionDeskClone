b51ffa37024fcfd8c86e85eef814495e650ff9ef
hiteshcrest <hitesh.p@crestinfosystems.com>
2021-06-02 16:08:54 +0530

    borrower verification changes

--- application/modules/frontend/views/emails/thank_you_escrow.php
+++ application/modules/frontend/views/emails/thank_you_escrow.php
@@ -1185,7 +1185,11 @@
 
                                                                         <td valign="top" class="mcnTextContent" style="padding-top:0; padding-right:18px; padding-bottom:9px; padding-left:18px;">
 
+<<<<<<< HEAD
                                                                             PACIFIC COAST TITLE COMPANY
+=======
+                                                                            <em>PACIFIC COAST TITLE COMPANY</em>
+>>>>>>> a79dac1c... borrower verification changes
                                                                             <br>
                                                                              © <?php echo date('Y'); ?> All rights reserved.
                                                                             <br>
