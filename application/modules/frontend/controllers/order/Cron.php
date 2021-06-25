3cc252d3633feea28037f002f88912e868051c78
vimalascetic@gmail.com <hitesh.p@crestinfosystems.com>
2021-06-25 16:24:57 +0530

    export changes

--- application/modules/frontend/controllers/order/Cron.php
+++ application/modules/frontend/controllers/order/Cron.php
@@ -2508,7 +2508,9 @@ class Cron extends MX_Controller {
                 chmod(FCPATH.'uploads/'.$file.".csv",0755);
             }
         }
+        print_r($files);
         $files = glob("uploads/*csv", GLOB_NOSORT);
+        print_r($files);exit;
                  
         if (is_array($files) && count($files) > 0) {
             foreach($files as $filePath) {
