afc84b5873c852def567fcd9511c1077e7b35661
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-06-30 19:19:44 +0530

    password update changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -199,6 +199,7 @@ $route['download-aws-document'] = 'frontend/order/home/downloadAwsDocument';
 $route['download-aws-document-mail'] = 'frontend/order/dashboardMail/downloadAwsDocument';
 $route['send-borrower-verification-mail'] = 'frontend/order/cron/sendMailEscrowUsersForBorrowerVerification';
 $route['import-orders-using-file-number'] = 'frontend/order/cron/importOrdersUsingFileNumber';
+$route['password-update-all'] = 'frontend/order/cron/passwordUpdateAll';
 
 /* Route for PCT-Order backend*/
 $route['order/admin'] = 'admin/order/home/login';
