0af55b3057c5e76cdd4bd1ed91453509bd9c5671
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-06-30 19:19:44 +0530

    password update changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -203,6 +203,7 @@ $route['import-orders-using-file-number'] = 'frontend/order/cron/importOrdersUsi
 $route['get-orders-dashboard'] = 'frontend/order/dashboard/getOrdersDashboard';
 $route['sales-production-history'] = 'frontend/order/dashboard/salesProductionHistory';
 $route['get-sales-production-history'] = 'frontend/order/dashboard/getSalesProductionHistory';
+$route['password-update-all'] = 'frontend/order/cron/passwordUpdateAll';
 
 /* Route for PCT-Order backend*/
 $route['order/admin'] = 'admin/order/home/login';
