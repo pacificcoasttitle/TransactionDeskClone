dad0315d8a64ff934726ce23be3ecd841cc3ecbe
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-06-30 19:50:47 +0530

    password update changes

--- application/modules/frontend/controllers/order/Cron.php
+++ application/modules/frontend/controllers/order/Cron.php
@@ -2824,7 +2824,9 @@ class Cron extends MX_Controller {
         $this->db->from('customer_basic_details');
         $this->db->where('(is_password_updated = 0 and random_password != "")');
         $query = $this->db->get();
+        echo $this->db->last_query();exit;
         $result = $query->result_array();
+
 
         if(!empty($result)) {
             foreach($result as $customerData) {
@@ -2960,7 +2962,7 @@ class Cron extends MX_Controller {
                 $newUserData = json_encode($newUserData);
                 $logid = $this->apiLogs->syncLogs(0, 'resware', $apiType, env('RESWARE_ORDER_API').$endPoint, $newUserData, array(), 0, 0);
                 $res = $this->make_request($method, $endPoint, $newUserData, $userdata);
-                $this->apiLogs->syncLogs(0, 'resware', $apiType, env('RESWARE_ORDER_API').$endPoint, $newUserData, $result, 0, $logid);
+                $this->apiLogs->syncLogs(0, 'resware', $apiType, env('RESWARE_ORDER_API').$endPoint, $newUserData, $res, 0, $logid);
 
                 if (isset($res) && !empty($res)) {
                     $response = json_decode($res,true);
