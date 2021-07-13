35682c3fcd700719927638dd51b53cb68016d384
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-07-13 14:51:30 +0530

    sales person date issue

--- application/modules/frontend/controllers/order/Cron.php
+++ application/modules/frontend/controllers/order/Cron.php
@@ -2745,6 +2745,7 @@ class Cron extends MX_Controller {
                                             'random_number' => $randomString,
                                             'resware_closed_status_date' => $completed_date,
                                             'resware_status'=> strtolower($res['Status']['Name']),
+                                            'sent_to_accounting_date' => $completed_date
                                         );
                                         $this->home_model->insert($orderData,'order_details');
                                     }
