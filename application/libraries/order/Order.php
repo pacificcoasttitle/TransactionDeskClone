30ba4e3ba199989f932d89e223f1bc128274e7dc
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-07-20 10:53:04 +0530

    cpl branches changes

--- application/libraries/order/Order.php
+++ application/libraries/order/Order.php
@@ -1000,7 +1000,7 @@ class Order
     public function getProposedBranches()
     {
         $this->CI->db->select('*');
-        $this->CI->db->from('pct_order_westcore_token');
+        $this->CI->db->from('pct_order_westcore_branches');
         $this->CI->db->where('is_proposed_branch', 1);
         $query = $this->CI->db->get();
         $result = $query->result_array();
@@ -1010,7 +1010,7 @@ class Order
     public function getProposedBranchDetail($branchId)
     {
         $this->CI->db->select('*');
-        $this->CI->db->from('pct_order_westcore_token');
+        $this->CI->db->from('pct_order_westcore_branches');
         $this->CI->db->where('id', $branchId);
         $query = $this->CI->db->get();
         $result = $query->row_array();
