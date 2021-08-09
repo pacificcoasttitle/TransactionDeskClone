f775c121ecf60fec3cf84d83277b214ad60cf4b5
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-08-09 11:54:30 +0530

    prelim issue

--- application/modules/frontend/controllers/order/ReviewPrelim.php
+++ application/modules/frontend/controllers/order/ReviewPrelim.php
@@ -205,6 +205,7 @@ class ReviewPrelim extends MX_Controller {
 				if(isset($order_details) && !empty($order_details)) {
 					$file_id = isset($order_details['file_id']) && !empty($order_details['file_id']) ? $order_details['file_id'] : '';
 					$orderDetails = $this->order->get_order_details($file_id,1);
+					
 					$parcelID = isset($data['ParcelID']) && !empty($data['ParcelID']) ? $data['ParcelID'] : '';
 					$vesting = isset($data['Vesting']) && !empty($data['Vesting']) ? $data['Vesting'] : '';
 					$generated_date = isset($data['CommitmentEffectiveDate']) && !empty($data['CommitmentEffectiveDate']) ? date('Y-m-d H:i:s', strtotime($data['CommitmentEffectiveDate'])) : '';
