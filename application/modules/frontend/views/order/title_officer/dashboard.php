8fad74cd4c0e668374fb71e8b5a5f00905a6af83
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-09-17 11:05:59 +0530

    title officer changes

--- application/modules/frontend/views/order/title_officer/dashboard.php
+++ application/modules/frontend/views/order/title_officer/dashboard.php
@@ -2,8 +2,7 @@
 	<?php
         $this->load->view('layout/header_dashboard');
     ?>
-    <style type="text/css">
-
+	<style type="text/css">
 		table#orders_listing tr td:last-child {
 			display: inline-flex;
 		}
@@ -12,51 +11,51 @@
 			max-height: 300px !important;
 		}
 
-    	#orders_listing_filter {
-    		display: inline-flex;
-    		float: right;
-    	}
+		#orders_listing_filter {
+			display: inline-flex;
+			float: right;
+		}
 
-    	select#month_filter {
-    		margin-bottom: 0px;
-		    margin-left: 0.5em;
-		    border: 1px solid #cbd2d6;
-		    border-radius: 3px;
-		    padding: 9px 22px 12px;
-    	}
+		select#month_filter {
+			margin-bottom: 0px;
+			margin-left: 0.5em;
+			border: 1px solid #cbd2d6;
+			border-radius: 3px;
+			padding: 9px 22px 12px;
+		}
 
 		select#orders_filter {
-    		margin-bottom: 0px;
-		    margin-left: 0.5em;
-		    border: 1px solid #cbd2d6;
-		    border-radius: 3px;
-		    padding: 9px 22px 12px;
-    	}
+			margin-bottom: 0px;
+			margin-left: 0.5em;
+			border: 1px solid #cbd2d6;
+			border-radius: 3px;
+			padding: 9px 22px 12px;
+		}
 
-    	.button-color {
-		    color: #888888;
+		.button-color {
+			color: #888888;
 		}
 
 		td.dataTables_empty {
 			display: table-cell !important;
 		}
 
-		.modal-dialog{
-		    overflow-y: initial !important
-		}
-		
-		.modal-body{
-		    height: 700px;
-		    overflow-y: auto;
+		.modal-dialog {
+			overflow-y: initial !important
 		}
 
-		.square-box{
+		.modal-body {
+			height: 700px;
+			overflow-y: auto;
+		}
+
+		.square-box {
 			background-color: #f0f0f0;
 			width: 23% !important;
 			margin-right: 2%;
 			margin-bottom: 50px;
 			padding-bottom: 35px;
-	    	padding-top: 15px;
+			padding-top: 15px;
 		}
 
 		.order-count-cotainer {
@@ -89,16 +88,16 @@
 			line-height: 23px;
 			color: #a0a0a0;
 		}
-		
+
 		.salesdivider {
 			border-bottom: 1px solid #fff;
-            padding-top: 20px;
-            padding-bottom: 20px;
+			padding-top: 20px;
+			padding-bottom: 20px;
 		}
 
 		.projected_goal_section {
-    		color: #d35411;
-    		font-weight: bold;
+			color: #d35411;
+			font-weight: bold;
 			text-align: center;
 			text-transform: uppercase;
 			font-size: large;
@@ -109,7 +108,7 @@
 			margin-bottom: 20px;
 		}
 
-    </style>
+	</style>
 	<section class="section-type-4a section-defaulta" style="padding-bottom:0px;">
 		<div class="container">
 			<div class="row">
@@ -118,7 +117,36 @@
 						<div class="typography-section__inner">
 							<h2 class="ui-title-block ui-title-block_light">Welcome Back <?php echo $name; ?>,</h2>
 							<div class="ui-decor-1a bg-accent"></div>
-							<h4 class="ui-title-block_light">Below is your production figures for the current month of <b><?php echo date('F');?></b></h3>
+							<div class="typography-sectiona">
+								<div class="col-md-12">
+									<a href="">
+										<button class="btn1 btn-type-1a btn-lg" type="button">Blank Forms</button>
+									</a>
+									<a href="<?php echo base_url().'cpl-dashboard'; ?>">
+										<button class="btn1 btn-type-1b btn-lg" type="button">Generate CPL</button>
+									</a>
+									<a href="<?php echo base_url().'proposed-insured'; ?>">
+										<button class="btn1 btn-type-1e btn-lg" type="button">Proposed</button>
+									</a>
+								</div>
+							</div>
+
+							<div class="typography-sectionc">
+								<div class="col-md-12">
+									<a href="<?php echo base_url().'fees'; ?>">
+										<button class="btn1 btn-type-1g btn-lg" type="button">Add Notes</button>
+									</a>
+									
+									<a href="<?php echo base_url().'prelim-files'; ?>">
+										<button class="btn1 btn-type-1c btn-lg" type="button">Review Prelim</button>
+									</a>
+									<a href="<?php echo base_url().'attach-files'; ?>">
+										<button class="btn1 btn-type-1d btn-lg" type="button">Upload Doc</button>
+									</a>
+								</div>
+							</div>
+							<h4 class="ui-title-block_light">Below is your production figures for the current month of
+								<b><?php echo date('F');?></b></h3>
 						</div>
 						<div class="order-count-cotainer">
 							<div class="col-md-3 title">Title Openings MTD</div>
@@ -127,48 +155,77 @@
 							<div class="col-md-3 title">Closings Ratio Avg</div>
 
 							<div class="col-md-3 square-box">
-								<div class="sales_loan_count" id="open_order_count"><?Php echo $total_open_count; ?></div>
-								<div class="salesdivider">
-								<div class="sales_loan_section">Sales = <span id="sale_open_count"><?Php echo $sale_open_count;?></span></div>
-								<div class="sales_loan_section">Refi's = <span id="refi_open_count"><?Php echo $refi_open_count;?></span></div>
+								<div class="sales_loan_count" id="open_order_count">
+									<?Php echo $total_open_count; ?>
 								</div>
-								<div style="margin-top: 20px;" class="projected_goal_section">Projected = <span id="projected_open_section"><?Php echo $projected_open_count;?></span></div>
-								<div class="projected_goal_section">Goal = <span id="goal_open_section"><?Php echo round($sales_rep_info['sales_rep_no_of_open_orders']/12);?></span></div>
+								<div class="salesdivider">
+									<div class="sales_loan_section">Sales = <span id="sale_open_count">
+											<?Php echo $sale_open_count;?></span></div>
+									<div class="sales_loan_section">Refi's = <span id="refi_open_count">
+											<?Php echo $refi_open_count;?></span></div>
+								</div>
+								<div style="margin-top: 20px;" class="projected_goal_section">Projected = <span
+										id="projected_open_section">
+										<?Php echo $projected_open_count;?></span></div>
+								<div class="projected_goal_section">Goal = <span id="goal_open_section">
+										<?Php echo round($sales_rep_info['sales_rep_no_of_open_orders']/12);?></span>
+								</div>
 							</div>
 
 							<div class="col-md-3 square-box">
-								<div class="sales_loan_count" id="close_order_count"><?Php echo $total_close_count; ?></div>
-								<div class="salesdivider">
-								<div class="sales_loan_section">Sales = <span id="sale_close_count"><?Php echo $sale_close_count;?></span></div>
-								<div class="sales_loan_section">Refi's = <span id="refi_close_count"><?Php echo $refi_close_count;?></span></div>
+								<div class="sales_loan_count" id="close_order_count">
+									<?Php echo $total_close_count; ?>
 								</div>
-								<div style="margin-top: 20px;" class="projected_goal_section">Projected = <span id="projected_close_section"><?Php echo $projected_close_count;?></span></div>
-								<div class="projected_goal_section">Goal = <span id="goal_close_section"><?Php echo round($sales_rep_info['sales_rep_no_of_close_orders']/12);?></span></div>
+								<div class="salesdivider">
+									<div class="sales_loan_section">Sales = <span id="sale_close_count">
+											<?Php echo $sale_close_count;?></span></div>
+									<div class="sales_loan_section">Refi's = <span id="refi_close_count">
+											<?Php echo $refi_close_count;?></span></div>
+								</div>
+								<div style="margin-top: 20px;" class="projected_goal_section">Projected = <span
+										id="projected_close_section">
+										<?Php echo $projected_close_count;?></span></div>
+								<div class="projected_goal_section">Goal = <span id="goal_close_section">
+										<?Php echo round($sales_rep_info['sales_rep_no_of_close_orders']/12);?></span>
+								</div>
 							</div>
 
 							<div class="col-md-3 square-box">
-								<div class="sales_loan_count">$<span id="total_premium"><?php echo number_format($total_premium); ?></span></div>
+								<div class="sales_loan_count">$<span
+										id="total_premium"><?php echo number_format($total_premium); ?></span></div>
 								<div class="salesdivider">
-								<div class="sales_loan_section">Sales = $<span id="sale_total_premium"><?php echo number_format($sale_total_premium); ?></span></div>
-								<div class="sales_loan_section">Refi's = $<span id="refi_total_premium"><?php echo number_format($refi_total_premium); ?></span></div>
+									<div class="sales_loan_section">Sales = $<span
+											id="sale_total_premium"><?php echo number_format($sale_total_premium); ?></span>
+									</div>
+									<div class="sales_loan_section">Refi's = $<span
+											id="refi_total_premium"><?php echo number_format($refi_total_premium); ?></span>
+									</div>
 								</div>
-								<div style="margin-top: 20px;" class="projected_goal_section">Projected = $<span id="projected_revenue_section"><?Php echo $projected_revenue;?></span></div>
-								<div class="projected_goal_section">Goal = $<span id="goal_revenue_section"><?Php echo round($sales_rep_info['sales_rep_premium']/12);?></span></div>
+								<div style="margin-top: 20px;" class="projected_goal_section">Projected = $<span
+										id="projected_revenue_section">
+										<?Php echo $projected_revenue;?></span></div>
+								<div class="projected_goal_section">Goal = $<span id="goal_revenue_section">
+										<?Php echo round($sales_rep_info['sales_rep_premium']/12);?></span></div>
 							</div>
 
 							<div class="col-md-3 square-box">
-								<div class="sales_loan_count"><span id="close_order_percetage"><?Php echo $close_order_percetage;?></span>%</div>
+								<div class="sales_loan_count"><span id="close_order_percetage">
+										<?Php echo $close_order_percetage;?></span>%</div>
 								<div class="salesdivider">
-								<div class="sales_loan_section">Sales = <span id="sale_close_order_percetage"><?Php echo $sale_close_order_percetage;?></span>%</div>
-								<div class="sales_loan_section">Refi's = <span id="refi_close_order_percetage"><?Php echo $refi_close_order_percetage;?></span>%</div>
+									<div class="sales_loan_section">Sales = <span id="sale_close_order_percetage">
+											<?Php echo $sale_close_order_percetage;?></span>%</div>
+									<div class="sales_loan_section">Refi's = <span id="refi_close_order_percetage">
+											<?Php echo $refi_close_order_percetage;?></span>%</div>
 								</div>
-								<div style="margin-top: 20px;" class="projected_goal_section">Projected = <span id="refi_open_count">0%</span></div>
+								<div style="margin-top: 20px;" class="projected_goal_section">Projected = <span
+										id="refi_open_count">0%</span></div>
 								<div class="projected_goal_section">Goal = <span id="refi_open_count">0%</span></div>
 							</div>
 
-							<h4 class="ui-title-block_light">Below is list of all your files. You can search for files by month that have a status open, closed, or cancelled.</h3>
-						</div>	
-						
+							<h4 class="ui-title-block_light">Below is list of all your files. You can search for files
+								by month that have a status open, closed, or cancelled.</h3>
+						</div>
+
 						<div class="typography-sectiona">
 							<div class="col-md-12">
 								<div class="table-container">
@@ -192,40 +249,42 @@
 			</div>
 		</div>
 	</section>
-    <div class="modal" id="partnersModal">
-	  <div class="modal-dialog modal-lg">
-	    <div class="modal-content">
-			<div class="modal-header">
-				<h4 class="modal-title">Partners</h4>
+	<div class="modal" id="partnersModal">
+		<div class="modal-dialog modal-lg">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h4 class="modal-title">Partners</h4>
+				</div>
+
+				<div class="modal-body">
+					<table class="table table-striped" id="tbl-partners-data">
+						<thead>
+							<tr>
+								<th>PartnerID</th>
+								<th>PartnerTypeID</th>
+								<th>PartnerTypeName</th>
+								<th>PartnerName</th>
+							</tr>
+						</thead>
+						<tbody></tbody>
+					</table>
+				</div>
+
+				<div class="modal-footer">
+					<button type="button" class="btn btn-danger" data-dismiss="modal"
+						style="background: #d35411;">Close</button>
+				</div>
+
 			</div>
-
-	      	<div class="modal-body">
-	            <table class="table table-striped" id="tbl-partners-data">
-				    <thead>
-						<tr>
-							<th>PartnerID</th>
-							<th>PartnerTypeID</th>
-							<th>PartnerTypeName</th>
-							<th>PartnerName</th>
-						</tr>
-				    </thead>
-				    <tbody></tbody>
-			    </table>
-			</div>
-
-	      	<div class="modal-footer">
-	        	<button type="button" class="btn btn-danger" data-dismiss="modal" style="background: #d35411;">Close</button>
-	      	</div>
-
-	    </div>
-	  </div>
+		</div>
 	</div>
 	<?php $this->load->view('layout/footer'); ?>
 </body>
+
 </html>
 <script>
 	$(document).ready(function () {
-		var order_list='';
+		var order_list = '';
 		if ($('#orders_listing').length) {
 			order_list = $('#orders_listing').DataTable({
 				"paging": true,
@@ -238,32 +297,32 @@
 					},
 					"emptyTable": "Record(s) not found.",
 					"search": "",
-                },
+				},
 				initComplete: function () {
-					
-					
-                },
+
+
+				},
 				"dom": 'lf<"orders_listing_filter">rtip',
 				buttons: [],
 				"drawCallback": function () {
-					
+
 				},
 				"ordering": false,
 				"serverSide": true,
 				"ajax": {
-					url: base_url + "get-title-officer-orders", 
-					type: "post", 
-					data   : function( d ) {
-	                  d.status = $('#orders_filter').val();
-					  d.month = $('#month_filter').val();
-	                },
-					dataFilter: function(data) {
-						var json = jQuery.parseJSON( data );
+					url: base_url + "get-title-officer-orders",
+					type: "post",
+					data: function (d) {
+						d.status = $('#orders_filter').val();
+						d.month = $('#month_filter').val();
+					},
+					dataFilter: function (data) {
+						var json = jQuery.parseJSON(data);
 						var countingData = json.count_data;
 						json.recordsTotal = json.recordsTotal;
 						json.recordsFiltered = json.recordsFiltered;
 						json.data = json.data;
-						return JSON.stringify( json );
+						return JSON.stringify(json);
 					},
 					error: function (XMLHttpRequest, textStatus, errorThrown) {
 						if (parseInt(XMLHttpRequest.status) == 419) {
@@ -281,23 +340,26 @@
 				}
 			});
 
-			$("div#orders_listing_filter").append('<label><select style="width:auto;" name="month_filter" id="month_filter" class="custom-select custom-select-sm form-control form-control-sm"> <option value="01"> January </option><option value="02">February</option><option value="03">March</option><option value="04">April</option><option value="05">May</option><option value="06">June</option><option value="07">July</option><option value="08">August</option><option value="09">September</option><option value="10">October</option><option value="11">November</option><option value="12">December</option></select></label><label><select style="width:auto;" name="orders_filter" id="orders_filter" class="custom-select custom-select-sm form-control form-control-sm"> <option value="open"> Open </option><option value="closed">Closed</option><option value="cancelled">Cancelled</option></select></label>');
+			$("div#orders_listing_filter").append(
+				'<label><select style="width:auto;" name="month_filter" id="month_filter" class="custom-select custom-select-sm form-control form-control-sm"> <option value="01"> January </option><option value="02">February</option><option value="03">March</option><option value="04">April</option><option value="05">May</option><option value="06">June</option><option value="07">July</option><option value="08">August</option><option value="09">September</option><option value="10">October</option><option value="11">November</option><option value="12">December</option></select></label><label><select style="width:auto;" name="orders_filter" id="orders_filter" class="custom-select custom-select-sm form-control form-control-sm"> <option value="open"> Open </option><option value="closed">Closed</option><option value="cancelled">Cancelled</option></select></label>'
+				);
 
-    		var d = new Date(),
+			var d = new Date(),
 
 				m = d.getMonth(),
 
 				y = d.getFullYear();
 
-			$('#month_filter option:eq('+m+')').prop('selected', true);
+			$('#month_filter option:eq(' + m + ')').prop('selected', true);
 		}
 
-		$("#orders_filter").on("change", function(){
-	        order_list.ajax.reload();
-	    });
+		$("#orders_filter").on("change", function () {
+			order_list.ajax.reload();
+		});
 
-		$("#month_filter").on("change", function(){
-	        order_list.ajax.reload();
-	    });
+		$("#month_filter").on("change", function () {
+			order_list.ajax.reload();
+		});
 	});
+
 </script>
