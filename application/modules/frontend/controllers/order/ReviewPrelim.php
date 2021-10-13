d58df7831b95b111fad688b8d743733fd0ab71d5
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-10-13 15:20:26 +0530

    sync changes

--- application/modules/frontend/controllers/order/ReviewPrelim.php
+++ application/modules/frontend/controllers/order/ReviewPrelim.php
@@ -18,40 +18,27 @@ class ReviewPrelim extends MX_Controller {
     
     public function fetchData()
     {
-		
     	ini_set('max_execution_time', 0); 
 		ini_set('memory_limit','2048M');
-		
 		$json = file_get_contents('php://input');
 
-		/*$url = "http://sandbox.pacificcoasttitle.com/resware-fetch-data";    
-		$content = $json;
-
-		$curl = curl_init($url);
-		curl_setopt($curl, CURLOPT_HEADER, false);
-		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-		curl_setopt($curl, CURLOPT_HTTPHEADER,
-				array("Content-type: application/json"));
-		curl_setopt($curl, CURLOPT_POST, true);
-		curl_setopt($curl, CURLOPT_POSTFIELDS, $content);
-
-		$json_response = curl_exec($curl);
-
-		$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-
-		if ( $status != 201 ) {
-			die("Error: call to URL $url failed with status $status, response $json_response, curl_error " . curl_error($curl) . ", curl_errno " . curl_errno($curl));
+		if ($_SERVER['SERVER_NAME'] == 'sandbox.pacificcoasttitle.com') {
+			$logSyncId = $this->apiLogs->syncLogs(0, 'local', 'sync_prelim_data','https://mypctrep.com/ReceiveSearchDataService.svc?wsdl', array('ReceiveSearchDataService'=>true), array());
+			$url = "http://app.pacificcoasttitle.com/resware-fetch-data";    
+			$curl = curl_init($url);
+			curl_setopt($curl, CURLOPT_HEADER, false);
+			curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($curl, CURLOPT_HTTPHEADER,
+					array("Content-type: application/json"));
+			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, $json);
+			$json_response = curl_exec($curl);
+			$status = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+			$this->apiLogs->syncLogs(0, 'local', 'sync_prelim_data', 'https://mypctrep.com/ReceiveSearchDataService.svc?wsdl', array(), $json_response, 0, $logSyncId);
+			curl_close($curl);
 		}
-
-
-		curl_close($curl);
-
-		$response = json_decode($json_response, true);
-		echo "here";exit;
-		print_r($response);exit;*/
-	
-		if ($json) 
-		{
+		
+		if ($json) {
 			$logId = $this->apiLogs->syncLogs(0,'resware WCF', 'get_prelim','https://mypctrep.com/ReceiveSearchDataService.svc?wsdl', array('ReceiveSearchDataService'=>true), array());
 			$this->apiLogs->syncLogs(0, 'resware WCF', 'get_prelim', 'https://mypctrep.com/ReceiveSearchDataService.svc?wsdl', array(), $json, 0, $logId);
 			$dir_name = APPPATH.'logs/prelim';
@@ -623,7 +610,6 @@ class ReviewPrelim extends MX_Controller {
 					else
 					{
 						$id = $this->reviewPrelimData->insert($summaryData);
-
 						$condition = array(
 								'file_number' => $file_number
 						);
@@ -1007,7 +993,7 @@ class ReviewPrelim extends MX_Controller {
 					$message = $prelim_message_body; 
 					$subject = 'The Prelim Hot Sheet';
 					//$to = $customer_email;
-					$to = 'hitesh.p@crestinfosystems.com';
+					//$to = 'hitesh.p@crestinfosystems.com';
 					
 					$this->load->helper('sendemail');
 					
