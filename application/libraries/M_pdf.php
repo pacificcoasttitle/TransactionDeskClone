c90ac21342721c6dacdd99628edbd359f09744ed
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-10-29 15:04:55 +0530

    label changes

--- application/libraries/M_pdf.php
+++ application/libraries/M_pdf.php
@@ -9,5 +9,17 @@ class M_pdf {
     	require_once(APPPATH."third_party/mpdf/mpdf.php");
         $this->param =$param;
         $this->pdf = new mPDF('','A4','','',10,10,10,10);
+        // $mpdf = new mPDF('',    // mode - default ''
+        //             '',    // format - A4, for example, default ''
+        //             0,     // font size - default 0
+        //             '',    // default font family
+        //             10,    // margin_left
+        //             10,    // margin right
+        //             30,     // margin top
+        //             30,    // margin bottom
+        //             9,     // margin header
+        //             9,     // margin footer
+        //             'L'  // L - landscape, P - portrait
+        //         );  
     }
 }
