8d7dcda1f9468f6f12679544b81af87bf4462b75
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-11-01 10:52:46 +0530

    login issue

--- application/modules/frontend/controllers/order/Cron.php
+++ application/modules/frontend/controllers/order/Cron.php
@@ -2643,9 +2643,9 @@ class Cron extends MX_Controller {
                         if(in_array('Sales Rep', $headerColumns)) {
                             $saleskey = array_search("Sales Rep",$headerColumns);
                             $salesRepName = $data[$saleskey];
-                            $salesRepName = str_replace(' ', '-', $salesRepName);
-                            $salesRepName = preg_replace('/[^A-Za-z0-9\-]/', '',  $salesRepName);
-                            $salesRepName = str_replace('-', ' ', $salesRepName);
+                            $salesRepName = str_replace(' ', '_', $salesRepName);
+                            $salesRepName = preg_replace('/[^A-Za-z0-9\_]/', '',  $salesRepName);
+                            $salesRepName = str_replace('_', ' ', $salesRepName);
                             $key = array_search($salesRepName, array_column($salesRepNameArr, 'name'));
                             if (isset($key) && !empty($key)) {
                                $salesRepId =  $salesRepNameArr[$key]['id'];
