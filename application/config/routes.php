3743a5c989ea6a9b4d76af8123acedb4a30c9a12
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-11-11 18:10:58 +0530

    payoff changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -218,6 +218,8 @@ $route['generate-payoff'] = 'frontend/order/payOff/generatePayoff';
 $route['add-partner-for-orders'] = 'frontend/order/cron/addPartnerForOrders';
 $route['import-data-for-pay-off'] = 'frontend/order/cron/importDataForPayOff';
 $route['get-pay-off-orders'] = 'frontend/order/payOff/get_pay_off_orders';
+$route['download-pay-off-document'] = 'frontend/order/payOff/downloadPayOffDocument';
+$route['update-pay-off-action'] = 'frontend/order/payOff/updatePayOffAction';
 
 /* Route for PCT-Order backend*/
 $route['order/admin'] = 'admin/order/login/login';
