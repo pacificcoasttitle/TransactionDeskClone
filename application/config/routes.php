b2789a130fd26821ae484b5fd5cd10b90a4b1ec0
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-11-11 19:03:23 +0530

    prelim changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -220,6 +220,7 @@ $route['import-data-for-pay-off'] = 'frontend/order/cron/importDataForPayOff';
 $route['get-pay-off-orders'] = 'frontend/order/payOff/get_pay_off_orders';
 $route['download-pay-off-document'] = 'frontend/order/payOff/downloadPayOffDocument';
 $route['update-pay-off-action'] = 'frontend/order/payOff/updatePayOffAction';
+$route['upload-document'] = 'frontend/order/common/upload_document';
 
 /* Route for PCT-Order backend*/
 $route['order/admin'] = 'admin/order/login/login';
