863f7cefbb697a3c8c3beb62945de190981c46f8
Hitesh Patel <hitesh.p@crestinfosystems.com>
2021-12-08 18:22:56 +0530

    partner changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -221,6 +221,7 @@ $route['get-pay-off-orders'] = 'frontend/order/payOff/get_pay_off_orders';
 $route['download-pay-off-document'] = 'frontend/order/payOff/downloadPayOffDocument';
 $route['update-pay-off-action'] = 'frontend/order/payOff/updatePayOffAction';
 $route['upload-document'] = 'frontend/order/common/upload_document';
+$route['add-partner-for-orders'] = 'frontend/order/cron/addPartnerForOrders';
 
 /* Route for PCT-Order backend*/
 $route['order/admin'] = 'admin/order/login/login';
