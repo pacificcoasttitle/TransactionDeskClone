8f0ad4d7525a256536399455448c6cd581bde56a
Hitesh Patel <hitesh.p@crestinfosystems.com>
2022-01-20 18:12:56 +0530

    line chart changes

--- application/libraries/order/Order.php
+++ application/libraries/order/Order.php
@@ -1404,7 +1404,7 @@ class Order
         }
     }
 
-    public function getOpenOrdersCountForRefiProducts($month, $userId)
+    public function getOpenOrdersCountForRefiProducts($month, $userId, $year = 0)
     {
         $userdata = $this->CI->session->userdata('user');
         $this->CI->db->select('count(*) as refi_count, sum(premium) as total_premium_for_refi_open_orders')
@@ -1413,7 +1413,12 @@ class Order
         //$this->CI->db->where('((order_details.resware_status != "closed" and order_details.resware_status != "cancelled") OR order_details.resware_status IS NULL)');
         $this->CI->db->where('order_details.prod_type', 'loan');
         $this->CI->db->where('MONTH(order_details.created_at)', $month); 
-        $this->CI->db->where('YEAR(order_details.created_at)', date('Y')); 
+        if ($year == 0) {
+            $this->CI->db->where('YEAR(order_details.created_at)', date('Y')); 
+        } else {
+            $this->CI->db->where('YEAR(order_details.created_at)', $year); 
+        }
+        
 
         if ($userId != 'all') {
             $this->CI->db->where('transaction_details.sales_representative', $userId); 
@@ -1426,7 +1431,7 @@ class Order
         return $result;
     }
 
-    public function getOpenOrdersCountForSaleProducts($month, $userId)
+    public function getOpenOrdersCountForSaleProducts($month, $userId, $year = 0)
     {
         $userdata = $this->CI->session->userdata('user');
         $this->CI->db->select('count(*) as sale_count, sum(premium) as total_premium_for_sale_open_orders')
@@ -1435,7 +1440,11 @@ class Order
         //$this->CI->db->where('((order_details.resware_status != "closed" and order_details.resware_status != "cancelled") OR order_details.resware_status IS NULL)');
         $this->CI->db->where('order_details.prod_type', 'sale');
         $this->CI->db->where('MONTH(order_details.created_at)', $month); 
-        $this->CI->db->where('YEAR(order_details.created_at)', date('Y')); 
+        if ($year == 0) {
+            $this->CI->db->where('YEAR(order_details.created_at)', date('Y')); 
+        } else {
+            $this->CI->db->where('YEAR(order_details.created_at)', $year); 
+        }
 
         if ($userId != 'all') {
             $this->CI->db->where('transaction_details.sales_representative', $userId); 
@@ -1447,7 +1456,7 @@ class Order
         return $result;
     }
 
-    public function getClosedOrdersCountForRefiProducts($month, $userId)
+    public function getClosedOrdersCountForRefiProducts($month, $userId, $year = 0)
     {
         $userdata = $this->CI->session->userdata('user');
         $this->CI->db->select('count(*) as refi_count, sum(premium) as total_premium_for_refi_close_orders')
@@ -1456,7 +1465,13 @@ class Order
         //$this->CI->db->where('order_details.resware_status = "closed"');
         $this->CI->db->where('order_details.prod_type', 'loan');
         $this->CI->db->where('MONTH(order_details.sent_to_accounting_date)', $month); 
-        $this->CI->db->where('YEAR(order_details.sent_to_accounting_date)', date('Y')); 
+
+        if ($year == 0) {
+            $this->CI->db->where('YEAR(order_details.sent_to_accounting_date)', date('Y')); 
+        } else {
+            $this->CI->db->where('YEAR(order_details.sent_to_accounting_date)', $year); 
+        }
+        
        
         if ($userId != 'all') {
             $this->CI->db->where('transaction_details.sales_representative', $userId); 
@@ -1468,7 +1483,7 @@ class Order
         return $result;
     }
 
-    public function getClosedOrdersCountForSaleProducts($month, $userId)
+    public function getClosedOrdersCountForSaleProducts($month, $userId, $year = 0)
     {
         $userdata = $this->CI->session->userdata('user');
         $this->CI->db->select('count(*) as sale_count, sum(premium) as total_premium_for_sale_close_orders')
@@ -1477,7 +1492,13 @@ class Order
         //$this->CI->db->where('order_details.resware_status = "closed"');
         $this->CI->db->where('order_details.prod_type', 'sale');
         $this->CI->db->where('MONTH(order_details.sent_to_accounting_date)', $month); 
-        $this->CI->db->where('YEAR(order_details.sent_to_accounting_date)', date('Y')); 
+
+        if ($year == 0) {
+            $this->CI->db->where('YEAR(order_details.sent_to_accounting_date)', date('Y')); 
+        } else {
+            $this->CI->db->where('YEAR(order_details.sent_to_accounting_date)', $year); 
+        }
+        
         
         if ($userId != 'all') {
             $this->CI->db->where('transaction_details.sales_representative', $userId); 
--- application/modules/frontend/controllers/order/SalesRep.php
+++ application/modules/frontend/controllers/order/SalesRep.php
@@ -240,4 +240,59 @@ class SalesRep extends MX_Controller
 		$this->load->view('layout/head_dashboard',$data);
 		$this->load->view('order/sales_production_history', $data);
 	}
+
+    function trends()
+	{
+        $userdata = $this->session->userdata('user');
+		$userId = $this->uri->segment(2);
+		$data['sales_user_id']  = $userId;
+		$data['is_sales_rep_manager'] = $userdata['is_sales_rep_manager'];
+
+		if ($userdata['is_sales_rep_manager'] == 1) {
+			$data['salesUsers'] = $this->order->get_sales_users();
+		} else {
+            if ($userId != $userdata['id']) {
+                redirect(base_url().'sales-production-history/'.$userdata['id']);
+            }
+			$data['salesUsers'] = array();
+		}
+		$data['title'] = 'Trends | Pacific Coast Title Company';
+        $salesHistory = array();
+
+        for ($year = (int)date('Y'); $year > (int)date('Y')- 2; $year--) {
+            $monthLimit = ($year == (int)date('Y')) ? (int)date('m') : 12; 
+            
+            for ($iM = 1; $iM <= $monthLimit; $iM++) {
+                $month = date("m", strtotime("$iM/12/10"));
+                $dateObj   = DateTime::createFromFormat('!m', $iM);
+                $monthName = $dateObj->format('F'); 
+                $salesHistory[$year][$iM-1]['month'] = $monthName;
+    
+                $openRefiResult = $this->order->getOpenOrdersCountForRefiProducts($month, $userId, strval( $year));
+                $refi_open_count = !empty($openRefiResult['refi_count']) ? $openRefiResult['refi_count'] : 0;
+                $openSaleResult = $this->order->getOpenOrdersCountForSaleProducts($month, $userId);
+                $sale_open_count = !empty($openSaleResult['sale_count']) ? $openSaleResult['sale_count'] : 0;
+                $salesHistory[$year][$iM-1]['total_open_count'] = $sale_open_count + $refi_open_count;
+    
+                $closeRefiResult = $this->order->getClosedOrdersCountForRefiProducts($month, $userId, strval( $year));
+                $refi_close_count = !empty($closeRefiResult['refi_count']) ? $closeRefiResult['refi_count'] : 0;
+                $closeSaleResult = $this->order->getClosedOrdersCountForSaleProducts($month, $userId);
+                $sale_close_count =  !empty($closeSaleResult['sale_count']) ? $closeSaleResult['sale_count'] : 0;
+                $salesHistory[$year][$iM-1]['total_close_count'] = $refi_close_count + $sale_close_count;
+    
+                $closeOrderRefiTotalPremium =  !empty($closeRefiResult['total_premium_for_refi_close_orders']) ? $closeRefiResult['total_premium_for_refi_close_orders'] : 0;
+                $refi_total_premium =  $closeOrderRefiTotalPremium;
+                $closeOrderSaleTotalPremium =  !empty($closeSaleResult['total_premium_for_sale_close_orders']) ? $closeSaleResult['total_premium_for_sale_close_orders'] : 0;
+                $sale_total_premium = $closeOrderSaleTotalPremium;
+                $salesHistory[$year][$iM-1]['total_premium'] = $sale_total_premium + $refi_total_premium;
+            }
+        }
+
+        //echo "<pre>";
+        //print_r($salesHistory[2022]);exit;
+		
+		$data['salesHistory'] = $salesHistory;
+        $this->load->view('layout/head_dashboard',$data);
+		$this->load->view('order/trends', $data);
+    }
 }
