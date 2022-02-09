a702e2e4b643c6722433c68c0f721facd8e424cc
Hitesh Patel <hitesh.p@crestinfosystems.com>
2022-02-09 16:17:31 +0530

    memo log changes

--- assets/backend/hr/js/custom.js
+++ assets/backend/hr/js/custom.js
@@ -7,6 +7,7 @@ var user_types = '';
 var departments = '';
 var positions = '';
 var memos = '';
+var memo_logs = '';
 
 $(document).ready(function () {
     
@@ -384,49 +385,8 @@ $(document).ready(function () {
         });
     } 
 
-    if ($('#memos').length > 0)  {
-        memos = $('#memos').DataTable({
-           "paging": true,
-            "lengthMenu": [10, 20, 50, 100, 200, 500, 1000],
-            "lengthChange": true,
-            "language": {
-                paginate: {
-                  next: '<i class="fa fa-chevron-right" aria-hidden="true"></i>',
-                  previous: '<i class="fa fa-chevron-left" aria-hidden="true"></i>',
-                },
-                "emptyTable": "Record(s) not found.",
-            },
-            initComplete: function() {
-            },
-            "dom": 'Blfrtip',
-            "drawCallback": function () {               
-                $('.dataTables_paginate > .pagination li').addClass('page-item');
-                $('.dataTables_paginate > .pagination a').addClass('page-link');
-                $('.dataTables_paginate > .pagination li.previous a, .dataTables_paginate > .pagination li.next a').addClass('rounded');
-            },
-            "ordering": false,            
-            "serverSide": true,
-            "ajax": {                
-                url: base_url+"hr/admin/get-memos", 
-                type: "post", 
-                error: function (XMLHttpRequest, textStatus, errorThrown) {
-                    if (parseInt(XMLHttpRequest.status) == 419) {
-                        alert("You are logged out. Please login.");
-                    }
-                    if (parseInt(XMLHttpRequest.status) == 419) {
-                        setTimeout(function () {
-                            location.reload();
-                        }, 1000);
-                    }
-                    $("#memos tbody").append('<tr><td colspan="6" class="text-center">No records found</td></tr>');
-                    $("#memos_processing").css("display", "none");
-                }
-            }            
-        });
-    } 
-
     if ($('#memo_logs').length > 0)  {
-        memos = $('#memo_logs').DataTable({
+        memo_logs = $('#memo_logs').DataTable({
            "paging": true,
             "lengthMenu": [10, 20, 50, 100, 200, 500, 1000],
             "lengthChange": true,
