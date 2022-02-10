c429a534b7504fdf1cc26c2e82efba909f39bb03
Binita Desai <binita.d@crestinfosystems.net>
2022-02-10 18:28:49 +0530

    Added complete task functionality at Admin side

--- application/modules/admin/models/hr/Task_list_model.php
+++ application/modules/admin/models/hr/Task_list_model.php
@@ -3,5 +3,5 @@ class Task_list_model extends MY_Model
 {
     public $_table = 'pct_hr_employee_task_list';
     public $belongs_to = array( 'category' => array( 'model' => 'hr/task_list_category','primary_key' => 'category_id' ));
-
+	public $has_many = array( 'users' => array( 'model' => 'hr/pct_hr_employee_task_list_complete','primary_key' => 'task_id' ));
 }
