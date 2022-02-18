cd41d3a12c65fc595105f3b00e0badaee184c5d1
Hitesh Patel <hitesh.p@crestinfosystems.com>
2022-02-18 09:40:23 +0530

    notification changes

--- application/modules/frontend/views/hr/template.php
+++ application/modules/frontend/views/hr/template.php
@@ -43,7 +43,9 @@
                 var notificationClickFlag  = 0;
                 var newNotificationFlag    = 0;
 
-                var socket = io.connect('//'+'<?php echo $_SERVER['SERVER_ADDR'];?>'+':1337');
+                var socket = io.connect('//'+'<?php echo $_SERVER['SERVER_ADDR'];?>'+':1337', {
+                    transports: ['websocket', 'xhr-polling']
+                });
                 var user_id = <?php echo $userdata['id'];?>;
 
                 socket.on('connect', function () {
