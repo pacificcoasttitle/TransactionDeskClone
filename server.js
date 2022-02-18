4542421177586db556cd2845eeb499dc93515079
Hitesh Patel <hitesh.p@crestinfosystems.com>
2022-02-18 11:13:07 +0000

    Revert "notification changes (pull request #1591)"

--- server.js
+++ server.js
@@ -1,12 +1,7 @@
 var server = require('http').createServer(),
+    io = require('socket.io')(server),
     logger = require('winston'),
     port = 1337;
-
-    var io = require('socket.io')(server, {
-        cors: {
-          origin: '*',
-        }
-      });
 
 // Logger config
 logger.remove(logger.transports.Console);
@@ -33,4 +28,3 @@ io.on('connection', function (socket){
 });
 
 server.listen(port);
-
