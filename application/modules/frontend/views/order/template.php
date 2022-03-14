8741e93d91179e9f7c68229abbbe959814786bfa
Hitesh Patel <hitesh.p@crestinfosystems.com>
2022-03-14 17:16:27 +0530

    notification changes

--- application/modules/frontend/views/order/template.php
+++ application/modules/frontend/views/order/template.php
@@ -144,7 +144,7 @@
                         } else if (notification.type == 'denied') {
                             alertClass = 'bg-danger';
                             iconClass = 'fa-ban';
-                        } else if (notification.type == 'accepted' || notification.type == 'assigned' || notification.type ==
+                        } else if (notification.type == 'added' || notification.type == 'assigned' || notification.type ==
                             'created') {
                             alertClass = 'bg-warning';
                             iconClass = 'fa-exclamation-triangle';
