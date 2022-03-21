c4e38207b378ef0c332388157fc50ad4f1ddc4b6
Hitesh Patel <hitesh.p@crestinfosystems.com>
2022-03-21 16:28:29 +0530

    calendar and training changes

--- application/modules/admin/controllers/hr/Dashboard.php
+++ application/modules/admin/controllers/hr/Dashboard.php
@@ -19,6 +19,7 @@ class Dashboard extends MX_Controller {
 	 * @see https://codeigniter.com/user_guide/general/urls.html
 	 */
 
+    private $dashboard_js_version = '01';
 	public function __construct()
     {
         parent::__construct();
@@ -28,6 +29,10 @@ class Dashboard extends MX_Controller {
         $this->load->library('form_validation');
         $this->load->library('hr/adminTemplate');
         $this->load->library('hr/common');
+        $this->load->model('hr/timecards_model');
+        $this->load->model('hr/vacation_request_model');
+        $this->load->model('hr/report_incident_model');
+        $this->load->model('hr/training_status_model');
         $this->common->is_hr_admin();
     }
 
@@ -35,7 +40,32 @@ class Dashboard extends MX_Controller {
     {
         $data['title'] = 'HR-Center Admin Dashboard';
         $data['page_title'] = 'Dashboard';
+        $data['pending_timecard_count'] = $this->timecards_model->count_by('approved_date', null);
+        $data['pending_vacation_request_count'] = $this->vacation_request_model->count_by('approved_date', null);
+        $data['pending_report_incident_count'] = $this->report_incident_model->count_by('approved_date', null);
+        $data['pending_training_count'] = $this->training_status_model->count_by('is_complete', 0);
+        $this->admintemplate->addCSS( base_url('assets/libs/calendar/main.css'));
+        $this->admintemplate->addJS( base_url('assets/libs/calendar/main.js'));
+        $this->admintemplate->addJS( base_url('assets/backend/hr/js/dashboard.js?v=dashboard_'.$this->dashboard_js_version) );
         $this->admintemplate->show("hr", "dashboard", $data);
+    }
+
+    public function getVacationDataForCalendar()
+    {
+        $start = date('Y-m-d', strtotime($this->input->post('start')));
+        $end = date('Y-m-d', strtotime($this->input->post('end')));
+        $vacationData = $this->common->getVacationDataForCalendar($start, $end);
+        $data = array();
+        $i = 0;
+        foreach ($vacationData as $vacation) {
+            $data[$i]['id'] = $vacation['id'];
+            $data[$i]['title'] = $vacation['first_name']." ".$vacation['last_name'];
+            $data[$i]['start'] = $vacation['from_date'];
+            $data[$i]['end'] = date('Y-m-d', strtotime($vacation['to_date'] . ' +1 day'));
+            $i++;
+        }
+        $i++;
+        echo json_encode($data); 
     }
 
     public function logout()
@@ -43,5 +73,4 @@ class Dashboard extends MX_Controller {
         $this->session->unset_userdata('hr_admin');
         redirect(base_url().'hr/admin');
     }
-    
 }
--- application/modules/admin/views/hr/dashboard.php
+++ application/modules/admin/views/hr/dashboard.php
@@ -1,3 +1,18 @@
+<style>
+  #calendar {
+    margin: 25px;
+	font-family: Arial, Helvetica Neue, Helvetica, sans-serif;
+    font-size: 14px;
+  }
+
+  #loading {
+    display: none;
+    position: absolute;
+    top: 10px;
+    right: 10px;
+  }
+</style>
+
 <!-- Begin Page Content -->
 <div class="container-fluid">
 
@@ -7,7 +22,6 @@
 
 	<!-- Content Row -->
 	<div class="row">
-
 		<!-- Earnings (Monthly) Card Example -->
 		<div class="col-xl-3 col-md-6 mb-4">
 			<div class="card border-left-primary shadow h-100 py-2">
@@ -15,11 +29,11 @@
 					<div class="row no-gutters align-items-center">
 						<div class="col mr-2">
 							<div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
-								Time Cards (Monthly)</div>
-							<div class="h5 mb-0 font-weight-bold text-gray-800">400</div>
+								Time Cards (Pending)</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $pending_timecard_count;?></div>
 						</div>
 						<div class="col-auto">
-							<i class="fas fa-calendar fa-2x text-gray-300"></i>
+							<i class="fas fa-clock fa-2x text-gray-300"></i>
 						</div>
 					</div>
 				</div>
@@ -33,11 +47,11 @@
 					<div class="row no-gutters align-items-center">
 						<div class="col mr-2">
 							<div class="text-xs font-weight-bold text-success text-uppercase mb-1">
-								Vacation Requests (Annual)</div>
-							<div class="h5 mb-0 font-weight-bold text-gray-800">400</div>
+								Vacation Requests (Pending)</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $pending_vacation_request_count;?></div>
 						</div>
 						<div class="col-auto">
-							<i class="fas fa-dollar-sign fa-2x text-gray-300"></i>
+							<i class="fas fa-table fa-2x text-gray-300"></i>
 						</div>
 					</div>
 				</div>
@@ -50,22 +64,11 @@
 				<div class="card-body">
 					<div class="row no-gutters align-items-center">
 						<div class="col mr-2">
-							<div class="text-xs font-weight-bold text-info text-uppercase mb-1">Tasks
-							</div>
-							<div class="row no-gutters align-items-center">
-								<div class="col-auto">
-									<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">50%</div>
-								</div>
-								<div class="col">
-									<div class="progress progress-sm mr-2">
-										<div class="progress-bar bg-info" role="progressbar" style="width: 50%"
-											aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-									</div>
-								</div>
-							</div>
+							<div class="text-xs font-weight-bold text-info text-uppercase mb-1">Report Incident</div>
+							<div class="h5 mb-0 mr-3 font-weight-bold text-gray-800"><?php echo $pending_report_incident_count;?></div>
 						</div>
 						<div class="col-auto">
-							<i class="fas fa-clipboard-list fa-2x text-gray-300"></i>
+							<i class="fas fa-file fa-2x text-gray-300"></i>
 						</div>
 					</div>
 				</div>
@@ -79,96 +82,30 @@
 					<div class="row no-gutters align-items-center">
 						<div class="col mr-2">
 							<div class="text-xs font-weight-bold text-warning text-uppercase mb-1">
-								Pending Requests</div>
-							<div class="h5 mb-0 font-weight-bold text-gray-800">18</div>
+								Pending Training</div>
+							<div class="h5 mb-0 font-weight-bold text-gray-800"><?php echo $pending_training_count;?></div>
 						</div>
 						<div class="col-auto">
-							<i class="fas fa-comments fa-2x text-gray-300"></i>
+							<i class="fas fa-sticky-note fa-2x text-gray-300"></i>
 						</div>
 					</div>
 				</div>
 			</div>
 		</div>
 	</div>
-
-	<!-- Content Row -->
 
 	<div class="row">
-
-		<!-- Area Chart -->
-		<div class="col-xl-8 col-lg-7">
+		<div class="col-xl-12">
+			
 			<div class="card shadow mb-4">
-				<!-- Card Header - Dropdown -->
-				<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-					<h6 class="m-0 font-weight-bold text-primary">Earnings Overview</h6>
-					<div class="dropdown no-arrow">
-						<a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"
-							aria-haspopup="true" aria-expanded="false">
-							<i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-						</a>
-						<div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-							aria-labelledby="dropdownMenuLink">
-							<div class="dropdown-header">Dropdown Header:</div>
-							<a class="dropdown-item" href="#">Action</a>
-							<a class="dropdown-item" href="#">Another action</a>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item" href="#">Something else here</a>
-						</div>
-					</div>
+				<div class="card-header py-3">
+					<h6 class="m-0 font-weight-bold text-primary">Vacation Requests Calendar</h6>
 				</div>
-				<!-- Card Body -->
-				<div class="card-body">
-					<div class="chart-area">
-						<canvas id="myAreaChart"></canvas>
-					</div>
-				</div>
-			</div>
-		</div>
-
-		<!-- Pie Chart -->
-		<div class="col-xl-4 col-lg-5">
-			<div class="card shadow mb-4">
-				<!-- Card Header - Dropdown -->
-				<div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-					<h6 class="m-0 font-weight-bold text-primary">Revenue Sources</h6>
-					<div class="dropdown no-arrow">
-						<a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"
-							aria-haspopup="true" aria-expanded="false">
-							<i class="fas fa-ellipsis-v fa-sm fa-fw text-gray-400"></i>
-						</a>
-						<div class="dropdown-menu dropdown-menu-right shadow animated--fade-in"
-							aria-labelledby="dropdownMenuLink">
-							<div class="dropdown-header">Dropdown Header:</div>
-							<a class="dropdown-item" href="#">Action</a>
-							<a class="dropdown-item" href="#">Another action</a>
-							<div class="dropdown-divider"></div>
-							<a class="dropdown-item" href="#">Something else here</a>
-						</div>
-					</div>
-				</div>
-				<!-- Card Body -->
-				<div class="card-body">
-					<div class="chart-pie pt-4 pb-2">
-						<canvas id="myPieChart"></canvas>
-					</div>
-					<div class="mt-4 text-center small">
-						<span class="mr-2">
-							<i class="fas fa-circle text-primary"></i> Direct
-						</span>
-						<span class="mr-2">
-							<i class="fas fa-circle text-success"></i> Social
-						</span>
-						<span class="mr-2">
-							<i class="fas fa-circle text-info"></i> Referral
-						</span>
-					</div>
-				</div>
+				<div id='loading'>loading...</div>	
+				<div id='calendar'></div>
 			</div>
 		</div>
 	</div>
-
-	
-
 </div>
 <!-- /.container-fluid -->
 
