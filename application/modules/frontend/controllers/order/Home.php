0add1e9b6eb502ce2b87c1e4e85f7387892a722c
Hitesh Patel <hitesh.p@crestinfosystems.com>
2022-04-13 11:47:58 +0530

    pct order client changes

--- application/modules/frontend/controllers/order/Home.php
+++ application/modules/frontend/controllers/order/Home.php
@@ -360,7 +360,7 @@ class Home extends MX_Controller {
 				
 					$loan['LienPosition'] = 0;
 				
-					$loan['LoanType'] = 'Conventional';
+					$loan['LoanType'] = 'ConvIns';
 				
 				$place_order['Loans'][] = $loan;
 				$place_order['Properties'][] = array('IsPrimary'=>'true', 'StreetNumber'=>$StreetNumber, 'StreetName'=> $StreetName, 'City'=> $PropertyCity, 'State'=> $PropertyState, 'County'=> $County, 'Zip'=>$PropertyZip);
