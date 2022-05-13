5c63ee4fc054d0eca080b364d484903baea0e6a0
Binita Desai <binita.d@crestinfosystems.net>
2022-05-13 19:01:58 +0530

    Commission range added

--- application/config/routes.php
+++ application/config/routes.php
@@ -318,6 +318,10 @@ $route['update-mortgage-user'] = 'admin/order/home/updateMortgageUser';
 $route['order/admin/mortgage-brokers'] = 'admin/order/home/mortgageBrokers';
 $route['is-mortgage-primary-user'] = 'admin/order/home/isMortgagePrimaryUser';
 $route['is-password-required'] = 'admin/order/home/isPasswordRequired';
+$route['order/admin/commission-range'] = 'admin/order/commissionRange/index';
+$route['order/admin/add-commission-range'] = 'admin/order/commissionRange/add_commission_range';
+$route['order/admin/edit-commission-range/(:num)'] = 'admin/order/commissionRange/edit_commission_range/$1';
+$route['order/admin/delete-commission-range/(:num)'] = 'admin/order/commissionRange/delete_commission_range/$1';
 
 
 $route['order/admin/get-realtors-list'] = 'admin/order/home/get_realtors_list';
---
+++ application/modules/admin/views/order/sales/commission_range.php
@@ -0,0 +1,59 @@
+<style>
+.dataTables_length {
+    width: 250px !important;
+    float: left;
+}
+</style>
+<div class="container-fluid">
+    <div class="card mb-3">
+        <div class="card-header">
+            <i class="fas fa-table"></i>
+            Sales Rep
+            <div class="float-right">
+                <a href="<?php echo base_url('order/admin/add-commission-range')?>" class="btn btn-secondary">Add Commission Range</a>
+            </div>
+        </div>
+     
+        <div class="card-body">
+		<?php if(!empty($success_msg)){ ?>
+			<div class="col-xs-12">
+				<div class="alert alert-success"><?php echo $success_msg; ?></div>
+			</div>
+		<?php } ?>
+		<?php if(!empty($error_msg)){ ?>
+			<div class="col-xs-12">
+				<div class="alert alert-danger"><?php echo $error_msg; ?></div>
+			</div>
+		<?php } ?>
+            <div class="table-responsive">
+                <table class="table table-bordered" id="tbl-commission-range-listing" width="100%" cellspacing="0">
+                    <thead>
+                        <tr>
+                            <th>Product Type</th>
+                            <th>Underwriter</th>
+                            <th>Minimum Revenue</th>
+                            <th>Maximum Revenue</th>
+                            <th>Total Commission</th>
+                            <th>Action</th>
+                        </tr>
+						
+                    </thead>                
+                    <tbody>
+					<?php foreach($commission_details as $commission_record): ?>
+						<tr>
+							<td><?php echo ucfirst($commission_record->product_type); ?></td>
+							<td><?php echo ucfirst($commission_record->underwriter); ?></td>
+							<td><?php echo $commission_record->min_revenue; ?></td>
+							<td><?php echo $commission_record->max_revenue; ?></td>
+							<td><?php echo $commission_record->total_commission; ?> %</td>
+							<td> <a href="<?php echo base_url('order/admin/edit-commission-range/'.$commission_record->id)?>" class='btn btn-action 'title ='Edit Commission Range'><span class='fa fa-edit' aria-hidden='true'></span></a>
+								<button type="button"  class='btn btn-action delete-record-custom' data-url="<?php echo base_url('order/admin/delete-commission-range/'.$commission_record->id)?>" title ='Delete Commission Range'><span class='fa fa-trash' aria-hidden='true'></span></button>
+							</td>
+						</tr>
+						<?php endforeach;?>
+					</tbody>
+                </table>
+            </div>
+        </div>
+    </div>
+</div>
