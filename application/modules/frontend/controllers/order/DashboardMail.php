a759f541dbb2a84562e46a720b3d954db5150d71
Hitesh Patel <hitesh.p@crestinfosystems.com>
2022-08-04 17:32:03 +0530

    escrow changes

--- application/modules/frontend/controllers/order/DashboardMail.php
+++ application/modules/frontend/controllers/order/DashboardMail.php
@@ -2545,6 +2545,7 @@ class DashboardMail extends MX_Controller {
             $pdfData['transferee_business_num'] = $this->input->post('transferee_business_num') ? $this->input->post('transferee_business_num') : array();
             $pdfData['calculation_amount'] = $this->input->post('calculation_amount') ? $this->input->post('calculation_amount') : array();
             $pdfData['perjury'] = $this->input->post('perjury') ? $this->input->post('perjury') : array();
+            $pdfData['docsInfo'] = $data['docsInfo'];
            
             $this->load->model('order/document');
             $document_name = "borrower_seller_".date('YmdHis')."_".$order[0]['file_id'].".pdf";
