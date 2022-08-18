4e4c0e1ec975cea1bff66a46d5d95a8909ae3dc8
Hitesh Patel <hitesh.p@crestinfosystems.com>
2022-08-18 13:47:29 +0530

    buyer changes

--- db/migrations/20220818080442_new_borrower_buyer_info.php
+++ db/migrations/20220818080442_new_borrower_buyer_info.php
@@ -18,6 +18,22 @@ final class NewBorrowerBuyerInfo extends AbstractMigration
      */
     public function change(): void
     {
-        
+        $table = $this->table('pct_order_borrower_buyer_info');
+        $table->rename('pct_order_borrower_buyer_packet_info')->update();
+
+        $table = $this->table('pct_order_borrower_buyer_info');
+        $table->addColumn('order_id', 'integer')
+            ->addColumn('first_name', 'string', ['null' => true])
+			->addColumn('last_name', 'string', ['null' => true])
+			->addColumn('email', 'string', ['null' => true])
+            ->addColumn('phone', 'string', ['null' => true])
+			->addColumn('birth_month', 'string', ['null' => true])
+            ->addColumn('birth_date', 'string', ['null' => true])
+			->addColumn('birth_year', 'string', ['null' => true])
+            ->addColumn('ssn', 'string', ['null' => true])
+			->addColumn('current_mailing_address', 'string', ['null' => true ])
+            ->addColumn('mailing_address_port_closing', 'string', ['null' => true ])
+			->addTimestamps()
+            ->create();
     }
 }
