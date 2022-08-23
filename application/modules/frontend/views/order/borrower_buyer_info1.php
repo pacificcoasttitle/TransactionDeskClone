1c6db0786167f425e17a7c1405a251220364f012
Binita Desai <binita.d@crestinfosystems.net>
2022-08-23 18:45:52 +0530

    PDF changes

--- application/modules/frontend/views/order/borrower_buyer_info1.php
+++ application/modules/frontend/views/order/borrower_buyer_info1.php
@@ -451,34 +451,34 @@
 								<div class="form-group">
 									<label for="email" class="form-label">Marital Status</label>
 									<select id="buyer[<?=$buyer['id']?>][marital_status]"
-										name="buyer[<?=$buyer['id']?>][marital_status]" required="required">
-										<option value="Select">Select</option>
-										<option value="husband_and_wife">Husband and Wife</option>
-										<option value="wife_and_husband">Wife and Husband</option>
-										<option value="a_married_couple">A Married Couple</option>
-										<option value="a_single_man">A Single Man (never married)</option>
-										<option value="a_single_woman">A Single Woman (never married)</option>
-										<option value="a_single_person">A Single Person (never married)</option>
-										<option value="a_married_man">A Married Man (as his sole and separate property)*
+										name="buyer[<?=$buyer['id']?>][marital_status]" required="required" class="buyer__show_hide_action has__data_val" data-action="married-to-option<?=$buyer['id']?>">
+										<option value="" data-val="0">Select</option>
+										<option value="husband_and_wife" data-val="1">Husband and Wife</option>
+										<option value="wife_and_husband" data-val="1">Wife and Husband</option>
+										<option value="a_married_couple" data-val="1">A Married Couple</option>
+										<option value="a_single_man" data-val="0">A Single Man (never married)</option>
+										<option value="a_single_woman" data-val="0">A Single Woman (never married)</option>
+										<option value="a_single_person" data-val="0">A Single Person (never married)</option>
+										<option value="a_married_man" data-val="0">A Married Man (as his sole and separate property)*
 										</option>
-										<option value="a_married_woman">A Married Woman (as her sole and separate
+										<option value="a_married_woman" data-val="0">A Married Woman (as her sole and separate
 											property)*</option>
-										<option value="a_married_person">A Married Person (as his/her sole and separate
+										<option value="a_married_person" data-val="0">A Married Person (as his/her sole and separate
 											property)*</option>
-										<option value="an_unmarried_man">An Unmarried Man (divorced)</option>
-										<option value="an_unmarried_woman">An Unmarried Woman (divorced)</option>
-										<option value="an_unmarried_person">An Unmarried Person (divorced)</option>
-										<option value="a_widow">A Widow (spouse deceased)</option>
-										<option value="a_widower">A Widower (spouse deceased)</option>
-										<option value="registered_domestic_partners">Registered Domestic Partners
+										<option value="an_unmarried_man" data-val="0">An Unmarried Man (divorced)</option>
+										<option value="an_unmarried_woman" data-val="0">An Unmarried Woman (divorced)</option>
+										<option value="an_unmarried_person" data-val="0">An Unmarried Person (divorced)</option>
+										<option value="a_widow" data-val="0">A Widow (spouse deceased)</option>
+										<option value="a_widower" data-val="0">A Widower (spouse deceased)</option>
+										<option value="registered_domestic_partners" data-val="1">Registered Domestic Partners
 										</option>
 									</select>
 								</div>
-								<div class="form-group">
+								<div class="form-group married-to-option<?=$buyer['id']?>" style="display: none;" >
 									<label for="email" class="form-label">Married To:</label>
 									<select class="married" id="buyer[<?=$buyer['id']?>][married_to]"
 										name="buyer[<?=$buyer['id']?>][married_to]">
-										<option value="Select">Select</option>
+										<option value="">Select</option>
 										<?php foreach($buyers as $key_buyer_married_to=>$buyer_married_to) :?>
 										<?php if($buyer_married_to['id'] != $buyer['id']) :?>
 										<option value="<?=$buyer_married_to['id']?>">
@@ -504,7 +504,7 @@
 									<label for="email" class="form-label">Marital Status</label>
 									<select id="buyer[new][marital_status]" name="buyer[new][marital_status]"
 										required="required">
-										<option value="Select">Select</option>
+										<option value="">Select</option>
 										<option value="husband_and_wife">Husband and Wife</option>
 										<option value="wife_and_husband">Wife and Husband</option>
 										<option value="a_married_couple">A Married Couple</option>
@@ -529,7 +529,7 @@
 								<div class="form-group">
 									<label for="email" class="form-label">Married To:</label>
 									<select id="buyer[new][married_to]" name="buyer[new][married_to]">
-										<option value="Select">Select</option>
+										<option value="">Select</option>
 										<?php foreach($buyers as $key_buyer_married_to=>$buyer_married_to) :?>
 
 										<option value="<?=$buyer_married_to['id']?>">
@@ -545,7 +545,7 @@
 							<div class="form-group">
 								<label class="form-label">Please tell us how the property will be vested:</label>
 								<select id="property_vested" name="property_vested" required="required">
-									<option value="Select">Select</option>
+									<option value="">Select</option>
 									<option value="community_property">Community Property</option>
 									<option value="community_property_with_right">Community Property with Right of
 										Survivorship</option>
