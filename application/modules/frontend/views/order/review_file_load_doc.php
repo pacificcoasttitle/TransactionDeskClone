6db1b2561233bc630645fdca6f8efa20c76e8f74
Hitesh Patel <hitesh.p@crestinfosystems.com>
2022-09-02 17:04:34 +0530

    hide button on prelim page

--- application/modules/frontend/views/order/review_file_load_doc.php
+++ application/modules/frontend/views/order/review_file_load_doc.php
@@ -5,8 +5,8 @@
 </style>
 <div class="typography-section__inner">
 	<a onclick="(<?php echo $api_document_id;?>, <?php echo $order_id;?>, '<?php echo $document_name;?>');" href="javascript:void(0);"><button class='btn btn-grad-2a' style='background: #d35411;' type='button'>Download</button></a>
-	<a style="display:<?php echo $prelim_flag == 1 ?  "contents" : "none";?>" onclick="download_document(<?php echo $api_document_id;?>, <?php echo $order_id;?>, '<?php echo $doc_document_name;?>');" href="javascript:void(0);"><button class='btn btn-grad-2a' style='width:auto;background: #d35411;' type='button'>Download Doc File</button></a>
-	<a style="display:<?php echo $prelim_flag == 1 ?  "contents" : "none";?>" onclick="upload_document(<?php echo $api_document_id;?>, <?php echo $order_id;?>, '<?php echo $doc_document_name;?>');" href="javascript:void(0);"><button class='btn btn-grad-2a' style='width:auto;background: #d35411;' type='button'>Upload Doc File</button></a>
+	<!-- <a style="display:<?php echo $prelim_flag == 1 ?  "contents" : "none";?>" onclick="download_document(<?php echo $api_document_id;?>, <?php echo $order_id;?>, '<?php echo $doc_document_name;?>');" href="javascript:void(0);"><button class='btn btn-grad-2a' style='width:auto;background: #d35411;' type='button'>Download Doc File</button></a>
+	<a style="display:<?php echo $prelim_flag == 1 ?  "contents" : "none";?>" onclick="upload_document(<?php echo $api_document_id;?>, <?php echo $order_id;?>, '<?php echo $doc_document_name;?>');" href="javascript:void(0);"><button class='btn btn-grad-2a' style='width:auto;background: #d35411;' type='button'>Upload Doc File</button></a> -->
 </div>
 
 <div class="l-main-content">
