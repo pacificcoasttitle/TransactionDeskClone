508683f15e73ec3a98958857d8e507c5dc0aaf5a
Hitesh Patel <hitesh.p@crestinfosystems.com>
2023-03-16 15:49:25 +0530

    lp changes

--- application/libraries/order/Titlepoint.php
+++ application/libraries/order/Titlepoint.php
@@ -1178,12 +1178,13 @@ class Titlepoint
                                 //echo "dcd".$existKey;
                                 //print_r($recordArray);
                                 if (strlen($existKey) > 0) {
-                                    //print_r($recordArray);
-                                   // echo "dcd---".$existKey."---".$val['DocumentType']."--------".$val['DocumentSubType'];
+                                    print_r($recordArray);
+                                   echo "dcd---".$existKey."---".$val['DocumentType']."--------".$val['DocumentSubType'];
                                     unset($recordArray[$existKey]);
                                     $recordArray = array_values($recordArray); 
-                                    //print_r($recordArray);
+                                    print_r($recordArray);
                                     $i--;
+                                    echo $i;
                                 }
                                 
                             }
@@ -1203,7 +1204,7 @@ class Titlepoint
                         }
                     }
                 }
-            
+            exit;
                 /** End All instrument number details fetched */
 
                 /* Start Address based instrument number details fetched  
