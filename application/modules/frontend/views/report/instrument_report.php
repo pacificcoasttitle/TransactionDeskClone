d72c0c48faec5e92d6cab7717be57383c0c2fcbf
crestpiyush <piyush.j@crestinfosystems.net>
2023-03-17 18:51:09 +0530

    Pre-listing report pdf and plat document

--- application/modules/frontend/views/report/instrument_report.php
+++ application/modules/frontend/views/report/instrument_report.php
@@ -1096,7 +1096,7 @@
             <div class="pdf_body">
                 <div class="table_title"><em>Section G:</em> Open Loans:</div>
                 <div style="height:20px"></div>
-                <img src="https://sandbox-pct.s3-us-west-2.amazonaws.com/plat-map/10275373.png" style="max-width:800px;margin:0 auto;text-align:center" alt=""/>
+                <img src="https://sandbox-pct.s3-us-west-2.amazonaws.com/plat-map/<?php echo $orderDetails['lp_file_number'] ?>.png" style="max-width:800px;margin:0 auto;text-align:center" alt=""/>
             </div>           
             <div class="pdf_footer">
                 <p class="page_title">Listing Prelim Report</p>
