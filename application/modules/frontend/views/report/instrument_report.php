7c1983c22f3410e3d2042ee86b213637449a0c94
crestpiyush <piyush.j@crestinfosystems.net>
2023-03-31 09:17:43 +0530

    Sales price added and date added in LP report

--- application/modules/frontend/views/report/instrument_report.php
+++ application/modules/frontend/views/report/instrument_report.php
@@ -715,7 +715,7 @@
                     </div>
                     <div class="float_right text_right">
                         Transaction: <?php echo ($orderDetails['transaction_type'] == 2) ? 'Loan' : 'Sale' ?><br>
-                        Price: $<?php echo ($orderDetails['transaction_type'] == 2) ? $orderDetails['loan_amount'] : $orderDetails['sale_amount']; ?>
+                        Price: $<?php echo ($orderDetails['transaction_type'] == 2) ? $orderDetails['loan_amount'] : $orderDetails['sales_amount']; ?>
                     </div>
                 </div>
                 <div class="clearfix"></div>
@@ -771,7 +771,7 @@
                 <div class="title_divider"></div>
                 <div class="f26">And its contents</div>
                 <div class="body_text">
-                    The information is available through public sources as of <b><span class="red_text">02/28/2023</span></b>. Information provided by the
+                    The information is available through public sources as of <b><span class="red_text"><?php echo date('m/d/Y', strtotime($orderDetails['opened_date'])); ?></span></b>. Information provided by the
                     vendor and public records may not always match exactly depending on how often the information is
                     updated by each source. The items presented or those found that are directly tied to the property in
                     question. Items that are directly associated with the owner will require a statement of
@@ -822,7 +822,7 @@
                     </div>
                     <div class="float_right text_right">
                         Transaction: <?php echo ($orderDetails['transaction_type'] == 2) ? 'Loan' : 'Sale' ?><br>
-                        Price: $<?php echo ($orderDetails['transaction_type'] == 2) ? $orderDetails['loan_amount'] : $orderDetails['sale_amount']; ?>
+                        Price: $<?php echo ($orderDetails['transaction_type'] == 2) ? $orderDetails['loan_amount'] : $orderDetails['sales_amount']; ?>
                     </div>
                 </div>
                 <div class="clearfix"></div>
