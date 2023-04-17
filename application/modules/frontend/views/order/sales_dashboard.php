b7e9df963822d4a95618b927957b3ba7c06c594d
crestpiyush <piyush.j@crestinfosystems.net>
2023-04-17 15:05:13 +0530

    Sales rep dashboard color decoration condiiton changed for production site and sales rep user email array updated

--- application/modules/frontend/views/order/sales_dashboard.php
+++ application/modules/frontend/views/order/sales_dashboard.php
@@ -309,7 +309,8 @@
 </div>
 
 <script defer>
-	var myarray = ["daphne@joindaphne.com", "info@retech.company", "Ccota@pct.com"];
+	var myarray = ["info@retech.company", "awu@pct.com", "teammeza@pct.com"];
+
 	var sessionValue = <?php echo json_encode($this->session->userdata('user'));?>;
 	if(myarray.includes(sessionValue.email)) {
 		setTimeout(() => {
