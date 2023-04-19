ce4943cf1404bdd6fb3481d938aad64bec6330ea
crestpiyush <piyush.j@crestinfosystems.net>
2023-04-19 09:29:01 +0530

    LP Alert prod changes

--- application/modules/frontend/controllers/order/SalesRep.php
+++ application/modules/frontend/controllers/order/SalesRep.php
@@ -304,7 +304,7 @@ class SalesRep extends MX_Controller
                 $your_date = strtotime($order['created_at']);
                 $datediff = $now - $your_date;
                 $datediff = round($datediff / (60 * 60 * 24));
-                if ($_SERVER['SERVER_NAME'] != 'app.pacificcoasttitle.com' && !empty($order['lp_file_number'])) {
+                if (!empty($order['lp_file_number'])) {
                     foreach ($lpAlertRange as $key => $val) {
                         // if ($val['delete'] == 1) {
                         //     $nestedData[] = 'delete';
