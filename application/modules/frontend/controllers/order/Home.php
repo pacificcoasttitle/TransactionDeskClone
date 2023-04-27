4e39e6ffacd7eea164eb39118e1ad2b1b4c8cd26
crestpiyush <piyush.j@crestinfosystems.net>
2023-04-27 17:01:43 +0530

    Petric kane enabled only on prod

--- application/modules/frontend/controllers/order/Home.php
+++ application/modules/frontend/controllers/order/Home.php
@@ -292,7 +292,7 @@ class Home extends MX_Controller {
 				}
 
 				$underWriter = '';
-				if (empty($_POST['EscrowId']) && empty($_POST['escrow_officer']) && ($orderUser['is_allow_only_resware_orders'] == 0) && ($orderUser['is_escrow'] == 0) && $ProductTypeID == '3') {
+				if (empty($_POST['EscrowId']) && empty($_POST['escrow_officer']) && ($SalesRep == '15340') && ($orderUser['is_allow_only_resware_orders'] == 0) && ($orderUser['is_escrow'] == 0) && $ProductTypeID == '3') {
 					$lpOrderFlag = 1;
 					$loanFlag = 1;
 					if(strpos($ProductTypeTxt, 'Sale') !== false) {
