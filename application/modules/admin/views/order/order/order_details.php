113b89bd14edadf887079cd9b4da1f49113321b7
crestpiyush <piyush.j@crestinfosystems.net>
2023-05-12 11:11:57 +0530

    design improvement and bug fixign

--- application/modules/admin/views/order/order/order_details.php
+++ application/modules/admin/views/order/order/order_details.php
@@ -515,7 +515,7 @@
                                 if(isset($order_details['additional_emails']) && !empty($order_details['additional_emails']))
                                 {
                             ?>
-                                    <div class="card mx-auto mt-5 mb-5" style="border-bottom: 1px solid rgba(0, 0, 0, 0.125);">
+                            <div class="card mx-auto mt-5 mb-5" style="border-bottom: 1px solid rgba(0, 0, 0, 0.125);">
                                 <div class="card-header" role="tab" id="deliverablesDetailsTab">
                                     <a data-toggle="collapse" class="collapsed" style="color: #000000;" data-parent="#accordionEx" href="#deliverablesDetails" aria-expanded="false"
                                     aria-controls="deliverablesDetails">
