d6b70b751d82ae06341c4587257c07237f65a390
Hitesh Patel <hitesh.p@crestinfosystems.com>
2023-05-12 17:27:05 +0530

    client changes

--- application/config/routes.php
+++ application/config/routes.php
@@ -410,6 +410,7 @@ $route['update-lp-document-is-ves-type-flag'] = 'admin/order/home/updateLpDocume
 $route['order/admin/regenerate-report'] = 'admin/order/home/regenerateReport';
 $route['order/admin/store-vesting-info'] = 'admin/order/home/addVestingInfo';
 $route['order/admin/get-vesting-info'] = 'admin/order/home/getVestingInfo';
+$route['order/admin/add-instrument-info'] = 'admin/order/home/addInstrumentInfo';
 /* Route for PCT-Order backend*/
 
 /* Start Route for HR-Center frontend */
