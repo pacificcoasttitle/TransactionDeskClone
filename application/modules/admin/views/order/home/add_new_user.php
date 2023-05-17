a7961934adceecd6c9e8b9d337647829ce6a438c
crestpiyush <piyush.j@crestinfosystems.net>
2023-05-17 13:49:06 +0530

    Sales rep commission pages design changes

--- application/modules/admin/views/order/home/add_new_user.php
+++ application/modules/admin/views/order/home/add_new_user.php
@@ -32,7 +32,7 @@
             <div class="col-md-12">
                 <div class="card shadow mb-4">
                     <div class="card-header py-3">
-                        <h6 class="m-0 font-weight-bold text-primary">Add Title Officer</h6>
+                        <h6 class="m-0 font-weight-bold text-primary">Add New User</h6>
                     </div>
                     <div class="card-body">        
                         <form id="add-new-user" method="POST">
--- application/modules/admin/views/order/sales/commission_bonus.php
+++ application/modules/admin/views/order/sales/commission_bonus.php
@@ -3,28 +3,48 @@
     width: 250px !important;
     float: left;
 }
+.fa-trash {
+    color: #223D7F;
+    text-decoration: none;
+    background-color: transparent;
+}
 </style>
+
 <div class="container-fluid">
-    <div class="card mb-3">
-        <div class="card-header">
-            <i class="fas fa-table"></i>
-            Bonus Ranges
-			<div class="float-right">
-				<a href="<?php echo base_url('order/admin/add-commission-bonus')?>" class="btn btn-secondary">Add Bonus</a>
-			</div>
+    <div class="row mb-3">
+		<div class="col-sm-6">
+			<h1 class="h3 text-gray-800">Bonus Ranges </h1>
+		</div>
+		<div class="col-sm-6">
+			<a href="<?php echo base_url('order/admin/add-commission-bonus')?>"  class="btn btn-success btn-icon-split float-right mr-2"> 
+                <span class="icon text-white-50">
+                    <i class="fas fa-plus"></i>
+                </span>
+                <span class="text"> Add Bonus </span> 
+            </a>
+		</div>
+	</div>
+    <!-- DataTables Example -->
+    <div class="card shadow mb-4">
+        <div class="card-header datatable-header py-3">
+            <div class="datatable-header-titles" > 
+                <span>
+                    <i class="fas fa-users"></i>
+                </span>
+                <h6 class="m-0 font-weight-bold text-primary pl-10">Bonus Ranges</h6> 
+            </div>
         </div>
-     
         <div class="card-body">
-		<?php if(!empty($success_msg)){ ?>
-			<div class="col-xs-12">
-				<div class="alert alert-success"><?php echo $success_msg; ?></div>
-			</div>
-		<?php } ?>
-		<?php if(!empty($error_msg)){ ?>
-			<div class="col-xs-12">
-				<div class="alert alert-danger"><?php echo $error_msg; ?></div>
-			</div>
-		<?php } ?>
+			<?php if(!empty($success_msg)){ ?>
+				<div class="col-xs-12">
+					<div class="alert alert-success"><?php echo $success_msg; ?></div>
+				</div>
+			<?php } ?>
+			<?php if(!empty($error_msg)){ ?>
+				<div class="col-xs-12">
+					<div class="alert alert-danger"><?php echo $error_msg; ?></div>
+				</div>
+			<?php } ?>
             <div class="table-responsive">
                 <table class="table table-bordered cusom__common__datatable" id="tbl-commission-bonus-listing" width="100%" cellspacing="0">
                     <thead>
@@ -43,7 +63,7 @@
 							<td><?php echo ($disp_key + 1); ?></td>
 							<td>$ <?php echo $commission_record->min_range; ?></td>
 							<td>$ <?php echo $commission_record->bonus_amount; ?></td>
-							<td> <a href="<?php echo base_url('order/admin/edit-commission-bonus/'.$commission_record->id)?>" class='btn btn-action 'title ='Edit Bonus'><span class='fas fa-edit' aria-hidden='true'></span></a>
+							<td> <a href="<?php echo base_url('order/admin/edit-commission-bonus/'.$commission_record->id)?>" title ='Edit Bonus'><span class='fas fa-edit' aria-hidden='true'></span></a>
 								<button type="button"  class='btn btn-action delete-record-custom' data-url="<?php echo base_url('order/admin/delete-commission-bonus/'.$commission_record->id)?>" title ='Delete Bonus'><span class='fas fa-trash' aria-hidden='true'></span></button>
 							</td>
 						</tr>
