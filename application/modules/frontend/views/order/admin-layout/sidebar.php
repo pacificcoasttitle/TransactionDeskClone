326a90561885a79c826cca538d106dcb1752136e
Hitesh Patel <hitesh.p@crestinfosystems.com>
2023-05-25 15:09:57 +0530

    sales new theme changes for all pages

--- application/modules/frontend/views/order/admin-layout/sidebar.php
+++ application/modules/frontend/views/order/admin-layout/sidebar.php
@@ -14,37 +14,42 @@
 
 	<!-- Divider -->
 	<hr class="sidebar-divider my-0">
-	<li class="nav-item <?php if($this->uri->segment(2) == 'sales-dashboard') { echo 'active'; } ?>">
+	<li class="nav-item <?php if($this->uri->segment(1) == 'sales-dashboard') { echo 'active'; } ?>">
 		<a class="nav-link" href="<?php echo base_url(); ?>sales-dashboard/<?php echo $userdata['id']; ?>">
 			<i class="fas fa fa-dashboard"></i>
 			<span>Dashboard</span>
 		</a>
 	</li>
-    <li class="nav-item <?php if($this->uri->uri_string() == 'sales-current-month-history') { echo 'active'; } ?>">
+    <li class="nav-item <?php if($this->uri->uri_string(1) == 'sales-current-month-history') { echo 'active'; } ?>">
 		<a class="nav-link" href="<?php echo base_url(); ?>sales-current-month-history">
 			<i class="fas fa fa-calendar  "></i>
 			<span>Daily</span>
 		</a>
 	</li>
-    <li class="nav-item <?php if($this->uri->segment(2) == 'sales-current-month-history') { echo 'active'; } ?>">
+    <li class="nav-item <?php if($this->uri->segment(1) == 'sales-production-history') { echo 'active'; } ?>">
 		<a class="nav-link" href="<?php echo base_url(); ?>sales-production-history/<?php echo $userdata['id']; ?>">
 			<i class="fas fa fa-history"></i>
 			<span>Production History</span>
 		</a>
 	</li>
-    <li class="nav-item <?php if($this->uri->segment(2) == 'trends') { echo 'active'; } ?>">
+    <li class="nav-item <?php if($this->uri->segment(1) == 'trends') { echo 'active'; } ?>">
 		<a class="nav-link" href="<?php echo base_url(); ?>trends/<?php echo $userdata['id']; ?>">
             <i class="fa fa-line-chart"></i>
 			<span>Trends</span>
 		</a>
 	</li>
-    <li class="nav-item <?php if($this->uri->segment(2) == 'sales-summary') { echo 'active'; } ?>">
+    <li class="nav-item <?php if($this->uri->segment(1) == 'sales-summary') { echo 'active'; } ?>">
 		<a class="nav-link" href="<?php echo base_url(); ?>sales-summary/<?php echo $userdata['id']; ?>">
 			<i class="fa fa-list-alt"></i>
 			<span>Summary</span>
 		</a>
 	</li>
-    
+    <li class="nav-item <?php if($this->uri->segment(1) == 'logout') { echo 'active'; } ?>">
+		<a class="nav-link" href="<?php echo base_url().'logout'; ?>">
+			<i class="fa fa-sign-out"></i>
+			<span>Logout</span>
+		</a>
+	</li>
 	
 
 	<!-- Divider -->
--- application/modules/frontend/views/order/sales_summary.php
+++ application/modules/frontend/views/order/sales_summary.php
@@ -48,11 +48,11 @@ td {
 }
 
 </style>
-<section class="section-type-4a section-defaulta b-contact b-contact_mod-a" style="padding-bottom:0px;">
-	<div class="content">
-		<div class="container">
-			<div class="row mb-3" style="margin-bottom:40px;">
-				<div class="col-sm-7">
+<div class="container-fluid">
+	<div class="card shadow mb-4">
+		<div class="card-body">
+			<div class="col-xs-12" style="display:flex;">
+				<div class="col-sm-8">
 					<div class="typography-section__inner">	
 						<div class="row">
 							<div class="">
@@ -60,23 +60,24 @@ td {
 							</div>
 						</div>
 					</div>
+					
 				</div>
-				<div class="col-sm-5 text-right custom__task_button">
-					<div class="typography-section__inner">
-						<?php if (!empty($salesUsers)) { ?>
-							<select name="sales_user_summary_filter" id="sales_user_summary_filter" class="custom-select custom-select-sm form-control form-control-sm select_user">
-								<?php foreach($salesUsers as $salesUser) { ?>
-									<option <?php echo ($sales_user_id == $salesUser['id']) ? 'selected' : '' ;?> value="<?php echo $salesUser['id'];?>"><?php echo $salesUser['first_name']." ".$salesUser['last_name'];?></option>
-								<?php }?>
-							</select>
-						<?php } ?>
-						<button type="button" class="btn button-color task_show_all"><i class="fa fa-plus"></i></button>
-						<button type="button" class="btn button-color task_hide_all"><i class="fa fa-minus"></i></button>
+				<div class="col-sm-4 text-right custom__task_button">
+						<div class="typography-section__inner">
+							<?php if (!empty($salesUsers)) { ?>
+								<select name="sales_user_summary_filter" id="sales_user_summary_filter" class="custom-select custom-select-sm form-control form-control-sm select_user">
+									<?php foreach($salesUsers as $salesUser) { ?>
+										<option <?php echo ($sales_user_id == $salesUser['id']) ? 'selected' : '' ;?> value="<?php echo $salesUser['id'];?>"><?php echo $salesUser['first_name']." ".$salesUser['last_name'];?></option>
+									<?php }?>
+								</select>
+							<?php } ?>
+							<button type="button" class="btn btn-primary btn-sm task_show_all"><i class="fa fa-plus"></i></button>
+							<button type="button" class="btn btn-primary btn-sm task_hide_all"><i class="fa fa-minus"></i></button>
+						</div>
+						<div class="mt-20">
+							
+						</div>
 					</div>
-					<div class="mt-20">
-						
-					</div>
-				</div>
 				<div id="result"></div>
 			</div>
 			
@@ -88,22 +89,22 @@ td {
 							<div class="card custom__task_card" style="background-color: #f2f2f2;">
 								<div class="card-header py-3">
 									<div class="row">
-										<div class="col-xs-3" style="padding-left: 40px;margin-top: 15px;">
+										<div class="col-md-3" style="padding-left: 40px;margin-top: 15px;">
 											<label class="custom-control custom-checkbox task__name">
 												<div class="check_box_text title_text"> SALES REP</div>
 											</label>
 										</div>
-										<div class="col-xs-5" style="padding-left: 40px;margin-top: 15px;">
+										<div class="col-md-5" style="padding-left: 40px;margin-top: 15px;">
 											<label class="custom-control custom-checkbox task__name">
 												<div class="check_box_text title_text"> COMPANY NAME</div>
 											</label>
 										</div>
-										<div class="col-xs-2" style="padding-left: 40px;margin-top: 15px;">
+										<div class="col-md-2" style="padding-left: 40px;margin-top: 15px;">
 											<label class="custom-control custom-checkbox task__name">
 												<div class="check_box_text title_text"> # OF DEALS</div>
 											</label>
 										</div>
-										<div class="col-xs-2 text-right" style="padding-left: 40px;margin-top: 15px;">
+										<div class="col-md-2 text-right" style="padding-left: 40px;margin-top: 15px;">
 											<a href="#collapseCard_<?php echo $summary['company_id']; ?>" class="custom__collapse_arrow collapsed" data-toggle="collapse"
 												role="button" aria-expanded="false" aria-controls="collapseCard_<?php echo $summary['company_id']; ?>">
 												<i class="fa fa-angle-down"></i>
@@ -120,24 +121,24 @@ td {
 										$keys = array();
 										$keys = array_keys(array_column($summary_info, 'parent_id'), $summary['company_id']);?>
 										<div class="card custom__task_card">
-											<div class="card-header py-3">
+											<div class="">
 												<div class="row">
-													<div class="col-xs-3" style="padding-left: 40px;margin-top: 15px;">
+													<div class="col-md-3" style="padding-left: 40px;margin-top: 15px;">
 														<label class="custom-control custom-checkbox task__name">
 															<div class="check_box_text"> <?php echo $summary['sales_name']; ?></div>
 														</label>
 													</div>
-													<div class="col-xs-5" style="padding-left: 40px;margin-top: 15px;">
+													<div class="col-md-5" style="padding-left: 40px;margin-top: 15px;">
 														<label class="custom-control custom-checkbox task__name">
 															<div class="check_box_text"> <?php echo $summary['company_name']; ?></div>
 														</label>
 													</div>
-													<div class="col-xs-2" style="padding-left: 40px;margin-top: 15px;">
+													<div class="col-md-2" style="padding-left: 40px;margin-top: 15px;">
 														<label class="custom-control custom-checkbox task__name">
 															<div class="check_box_text"> <?php echo $summary['num_of_deals']; ?></div>
 														</label>
 													</div>
-													<div class="col-xs-2 text-right" style="padding-left: 40px;margin-top: 15px;">
+													<div class="col-md-2 text-right" style="padding-left: 40px;margin-top: 15px;">
 														<a href="#collapseCard_<?php echo $summary['company_id']; ?>" class="custom__collapse_arrow collapsed" data-toggle="collapse"
 															role="button" aria-expanded="false" aria-controls="collapseCard_<?php echo $summary['company_id']; ?>">
 															<i class="fa fa-angle-down"></i>
@@ -151,7 +152,7 @@ td {
 													<div class="smart-forms spacer-b30 spacer-t30">
 														<div class="tagline"><span>Client Summary</span></div>
 													</div>
-													<table class="table table-type-3 typography-last-elem no-footer">
+													<table class="table table-bordered" width="100%" cellspacing="0">
 														<thead>
 															<tr>
 																<th>#</th>
@@ -198,7 +199,7 @@ td {
 		</div>
 		<div class="typography-sectionab"></div>
 	</div>
-</section>
+</div>
 
 
 
