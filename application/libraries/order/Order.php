dcc4fb8958f2e24bb7da6d2fe4998fd723aff8aa
crestpiyush <piyush.j@crestinfosystems.net>
2023-06-01 17:23:39 +0530

    Service failed condition added

--- application/libraries/order/Order.php
+++ application/libraries/order/Order.php
@@ -3673,7 +3673,7 @@ class Order
         $taxDocStatus = $titlePointDetails[0]['tax_file_status'];
         $emailSentFlag = $titlePointDetails[0]['email_sent_status'];
         $this->CI->apiLogs->syncLogs($userdata['id'], 'email-check', 'email-check', '', ['$emailSentFlag' => $emailSentFlag, '$taxDocStatus' => $taxDocStatus, '$lvDocStatus' => $lvDocStatus], array(), $orderDetails['order_id'], 0);
-        if ($emailSentFlag != 1  && $taxDocStatus == 'success' && $lvDocStatus == 'success') {
+        if ($emailSentFlag != 1  && $taxDocStatus == 'success' && $taxDocStatus == 'failed' && $lvDocStatus == 'success' && $lvDocStatus == 'failed')  {
             if (isset($orderDetails['lp_file_number']) && !empty($orderDetails['lp_file_number'])) {
                 $logid = $this->CI->apiLogs->syncLogs($userdata['id'], 'sendgrid', 'send_confirmation_LP_order_mail', '', $mailParams, array(), $orderDetails['order_id'], 0);
                 try {
