e007a5c46af1a6e12d7d745efd4d0ca22dfbc174
crestpiyush <piyush.j@crestinfosystems.net>
2023-06-02 11:24:46 +0530

    Execption condition added

--- application/modules/frontend/controllers/order/Cron.php
+++ application/modules/frontend/controllers/order/Cron.php
@@ -5528,11 +5528,11 @@ class Cron extends MX_Controller {
             )
         );
         $titlePointDetails = $this->titlePointData->gettitlePointDetails($condition);
-        $lvDocStatus = $titlePointDetails[0]['lv_file_status'];
-        $taxDocStatus = $titlePointDetails[0]['tax_file_status'];
-        $emailSentFlag = $titlePointDetails[0]['email_sent_status'];
+        $lvDocStatus = strtolower($titlePointDetails[0]['lv_file_status']);
+        $taxDocStatus = strtolower($titlePointDetails[0]['tax_file_status']);
+        $emailSentFlag = strtolower($titlePointDetails[0]['email_sent_status']);
         $this->apiLogs->syncLogs(0, 'email-check-LV', 'email-check-LV', '', ['$emailSentFlag' => $emailSentFlag, '$taxDocStatus' => $taxDocStatus, '$lvDocStatus' => $lvDocStatus], array(), 0, 0);
-        if ($emailSentFlag != 1  && ($taxDocStatus == 'success' || $taxDocStatus == 'failed') && ($lvDocStatus == 'success' || $lvDocStatus == 'failed')) 
+        if ($emailSentFlag != 1  && ($taxDocStatus == 'success' || $taxDocStatus == 'failed' || $taxDocStatus == 'exception') && ($lvDocStatus == 'success' || $lvDocStatus == 'failed' || $lvDocStatus == 'exception')) 
         {
             $this->order->sendOrderEmail($fileNumber);
             $this->session->set_userdata('email_sent_flag', 1);
--- application/modules/frontend/controllers/order/Home.php
+++ application/modules/frontend/controllers/order/Home.php
@@ -1186,10 +1186,10 @@ class Home extends MX_Controller {
 					'cc'=>json_encode($cc)
 				);
 				$titlePointDetails = $this->titlePointData->gettitlePointDetails($condition);
-				$lvDocStatus = $titlePointDetails[0]['lv_file_status'];
-				$taxDocStatus = $titlePointDetails[0]['tax_file_status'];
-				$emailSentFlag = $titlePointDetails[0]['email_sent_status'];
-				if ((!isset($orderDetails['lp_file_number']) || empty($orderDetails['lp_file_number'])) && $emailSentFlag != 1 && ($taxDocStatus == 'success' || $taxDocStatus == 'failed') && ($lvDocStatus == 'success' || $lvDocStatus == 'failed')) {
+				$lvDocStatus = strtolower($titlePointDetails[0]['lv_file_status']);
+				$taxDocStatus = strtolower($titlePointDetails[0]['tax_file_status']);
+				$emailSentFlag = strtolower($titlePointDetails[0]['email_sent_status']);
+				if ((!isset($orderDetails['lp_file_number']) || empty($orderDetails['lp_file_number'])) && $emailSentFlag != 1 && ($taxDocStatus == 'success' || $taxDocStatus == 'failed' || $taxDocStatus == 'exception') && ($lvDocStatus == 'success' || $lvDocStatus == 'failed' || $lvDocStatus == 'exception')) {
 					$to = 'hitesh.p@crestinfosystems.com';
 					$cc = ['piyush.j@crestinfosystems.net'];
 					$logid = $this->apiLogs->syncLogs($userdata['id'], 'sendgrid', 'send_confirmation_resware_order_mail', '', $mailParams, array(), $orderId, 0);
