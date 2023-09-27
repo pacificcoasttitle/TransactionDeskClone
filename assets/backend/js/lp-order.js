14eee26e3e55527ca10c0e3772bae2f25a9ab92f
Hitesh Patel <hitesh.p@crestinfosystems.com>
2023-09-27 17:24:15 +0530

    lp duplication flag

--- assets/backend/js/lp-order.js
+++ assets/backend/js/lp-order.js
@@ -371,3 +371,57 @@ function changeClient(file_id) {
     $('#client_file_id').val(file_id);
     $('#changeClientModel').modal('show');
 }
+
+function avoidDuplication()
+{    
+    $('input[type="checkbox"]').on('change', function() {
+        $('body').animate({ opacity: 0.5 }, "slow");
+        var property_id = $(this).attr('id');
+        if ($(this).is(":checked")) {
+            var avoidFlag = 1;
+        } else {
+            var avoidFlag = 0;
+        }
+        $.ajax({
+            url: base_url+"update-avoid-duplication-flag",
+            method: "POST",
+            data : {
+                property_id: property_id,
+                avoidFlag: avoidFlag
+            },
+            success: function(data){
+                var result = jQuery.parseJSON(data);
+                if (result.status == 'success') {
+                    $('body').animate({ opacity: 1.0 }, "slow");
+                    $('#lp_order_success_msg').html(result.msg).show();
+                    $([document.documentElement, document.body]).animate({
+                        scrollTop: $("#lp_order_success_msg").offset().top
+                    }, 1000);
+                    order_list.ajax.reload( null, false );
+                    setTimeout(function () {
+                        $('#lp_order_success_msg').html('').hide();
+                    }, 4000);
+                } else {
+                    $('#lp_order_error_msg').html(result.message).show();
+                    $([document.documentElement, document.body]).animate({
+                        scrollTop: $("#lp_order_error_msg").offset().top
+                    }, 1000);
+
+                    setTimeout(function () {
+                        $('#lp_order_error_msg').html('').hide();
+                    }, 4000);
+                }
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                $('#lp_order_error_msg').html('Something went wrong. Please try it again.').show();
+                $([document.documentElement, document.body]).animate({
+                    scrollTop: $("#lp_order_success_msg").offset().top
+                }, 1000);
+
+                setTimeout(function () {
+                    $('#lp_order_error_msg').html('').hide();
+                }, 4000);
+            }
+        });
+    });
+}
