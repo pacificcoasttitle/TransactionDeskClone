f0376dcff96fd129f3b0e7856462a88f15c620fd
Hitesh Patel <hitesh.p@crestinfosystems.com>
2023-10-06 14:58:16 +0530

    sales snap shot changes

--- application/modules/frontend/controllers/SalesSnapShot.php
+++ application/modules/frontend/controllers/SalesSnapShot.php
@@ -174,7 +174,7 @@ class SalesSnapShot extends MX_Controller {
                 $dir_name = str_replace('\\', '/', $dir_name);
 
                 $this->snappy_pdf->pdf->setOption('page-size', 'Letter');
-			    $this->snappy_pdf->pdf->setOption('zoom', '1.05');
+			  //  $this->snappy_pdf->pdf->setOption('zoom', '1.05');
                 $this->snappy_pdf->pdf->generateFromHtml($html,$dir_name.$document_name);
                 $response = $this->order->uploadDocumentOnAwsS3($document_name, 'sales-snap-shot');
                 if($response) {
