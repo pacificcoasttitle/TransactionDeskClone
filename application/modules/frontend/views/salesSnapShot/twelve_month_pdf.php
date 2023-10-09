960ceef66eb154f8611d33c6038eb634a773ea97
Hitesh Patel <hitesh.p@crestinfosystems.com>
2023-10-09 16:18:19 +0530

    sales snap shot changes

--- application/modules/frontend/views/salesSnapShot/twelve_month_pdf.php
+++ application/modules/frontend/views/salesSnapShot/twelve_month_pdf.php
@@ -12,7 +12,7 @@
                 <h1>SALES SNAP SHOT</h1>
                 <div class="overview_text">12 MONTH OVERVIEW</div>
                 <div class="santa_monica">SANTA MONICA</div>
-                <img src="<?php echo base_url('assets/sales_snap_shot/logo.png') ?>" class="pacific_logo" alt="">
+                <!-- <img src="<?php echo base_url('assets/sales_snap_shot/logo.png') ?>" class="pacific_logo" alt=""> -->
             </div>
             <div class="pdf_body">
                 <div class="grid">
