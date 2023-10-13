b2af5f6156d4078f44a417daaff9f9a6c300f054
Hitesh Patel <hitesh.p@crestinfosystems.com>
2023-10-13 18:44:37 +0530

    summary changes

--- application/libraries/order/Order.php
+++ application/libraries/order/Order.php
@@ -3817,7 +3817,6 @@ class Order
 
     public function sendSummaryMail($sales_rep_id = 0)
     {
-        echo $sales_rep_id;exit;
         $this->CI->load->model('order/apiLogs');
         $month = sprintf('%02d',date('m') - 1);
         $this->CI->db->select('order_details.file_id, 
