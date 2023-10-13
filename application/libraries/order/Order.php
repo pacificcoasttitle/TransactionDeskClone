9d542e61d1c8ef7a2fbf3a5daf0acf844bee2fdc
Hitesh Patel <hitesh.p@crestinfosystems.com>
2023-10-13 18:27:33 +0530

    summary changes

--- application/libraries/order/Order.php
+++ application/libraries/order/Order.php
@@ -2930,8 +2930,8 @@ class Order
                         'message' => json_encode($data),
                         'cc' => $cc
                     );
-                    $to = 'hitesh.p@crestinfosystems.com';
-                    $cc = array();
+                    //$to = 'ghernandez@pct.com';
+                    //$cc = array();
                     $this->CI->load->helper('sendemail');
                     $logid = $this->CI->apiLogs->syncLogs(0, 'sendgrid', 'send_mail_to_escrow_user', '', $mailParams, array(), 0, 0);
                     $escrow_mail_result = send_email($from_mail, $from_name, $to, $subject, $message, array(), $cc);
