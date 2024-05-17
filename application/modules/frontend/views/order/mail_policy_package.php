bf130325ab7c2dd0680be3f876312141c2a0a316
crestpiyush <piyush.j@crestinfosystems.net>
2024-05-17 12:37:43 +0530

    Js issue fixed

--- application/modules/frontend/views/order/mail_policy_package.php
+++ application/modules/frontend/views/order/mail_policy_package.php
@@ -134,6 +134,7 @@
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/frontend/css/font-awesome.min.css">
 <link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/frontend/css/jquery-ui.css">
 
+<script type="text/javascript" src="<?php echo base_url(); ?>assets/libs/jquery-1.12.4.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/frontend/js/jquery.form.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/frontend/js/jquery.validate.min.js"></script>
 <script type="text/javascript" src="<?php echo base_url(); ?>assets/frontend/js/additional-methods.min.js"></script>
