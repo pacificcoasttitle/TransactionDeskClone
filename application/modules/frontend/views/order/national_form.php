a74c11673469411844887777ad860d0994726828
crestpiyush <piyush.j@crestinfosystems.net>
2024-05-23 09:00:32 +0530

    Title of the page changed

--- application/modules/frontend/views/order/national_form.php
+++ application/modules/frontend/views/order/national_form.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<meta name="author" content="colorlib.com">
 	<meta http-equiv="X-UA-Compatible" content="ie=edge">
-	<title>Sign Up Form - Buyer</title>
+	<title><?=$title;?></title>
 
 	<!-- Font Icon -->
 	<link rel="stylesheet"
