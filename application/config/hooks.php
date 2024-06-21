9213d36cc4293e401b42e28f1658c38cfca66330
crestpiyush <piyush.j@crestinfosystems.net>
2024-06-21 16:30:34 +0530

    Environment setting in app

--- application/config/hooks.php
+++ application/config/hooks.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 /*
 | -------------------------------------------------------------------------
@@ -8,6 +8,14 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | This file lets you define "hooks" to extend CI without hacking the core
 | files.  Please see the user guide for info:
 |
-|	https://codeigniter.com/user_guide/general/hooks.html
+|    https://codeigniter.com/user_guide/general/hooks.html
 |
-*/
+ */
+
+$hook['post_controller_constructor'][] = array(
+    'class' => 'DatabaseErrorHook',
+    'function' => 'handle_db_error',
+    'filename' => 'DatabaseErrorHook.php',
+    'filepath' => 'hooks',
+    'params' => array(),
+);
