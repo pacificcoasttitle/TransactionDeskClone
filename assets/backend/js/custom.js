7412bec60c7398ce2602c07d265dd8955a9dc1d2
crestpiyush <piyush.j@crestinfosystems.net>
2024-11-25 16:47:02 +0530

    Add Partner condition added based on admin flag, Title officer flag for email recepient, Synced order on hovering LP order

--- assets/backend/js/custom.js
+++ assets/backend/js/custom.js
@@ -6481,3 +6481,56 @@ function isAllowOnlyReswareOrders() {
         });
     });
 }
+
+function updateTitleOfficerEmailReceiveFlag() {
+    $('input[type="checkbox"]').on('change', function () {
+        $('body').animate({ opacity: 0.5 }, "slow");
+        var title_officer_id = $(this).attr('id');
+        if ($(this).is(":checked")) {
+            var displayFlag = 1;
+        } else {
+            var displayFlag = 0;
+        }
+        $.ajax({
+            url: base_url + "update-title-officer-email-receive-flag",
+            method: "POST",
+            data: {
+                title_officer_id: title_officer_id,
+                displayFlag: displayFlag
+            },
+            success: function (data) {
+                var result = jQuery.parseJSON(data);
+                if (result.status == 'success') {
+                    $('body').animate({ opacity: 1.0 }, "slow");
+                    $('#title_officer_success_msg').html(result.msg).show();
+                    $([document.documentElement, document.body]).animate({
+                        scrollTop: $("#title_officer_success_msg").offset().top
+                    }, 1000);
+                    title_officer_list.ajax.reload(null, false);
+                    setTimeout(function () {
+                        $('#title_officer_success_msg').html('').hide();
+                    }, 4000);
+                } else {
+                    $('#title_officer_error_msg').html(result.message).show();
+                    $([document.documentElement, document.body]).animate({
+                        scrollTop: $("#title_officer_error_msg").offset().top
+                    }, 1000);
+
+                    setTimeout(function () {
+                        $('#title_officer_error_msg').html('').hide();
+                    }, 4000);
+                }
+            },
+            error: function (XMLHttpRequest, textStatus, errorThrown) {
+                $('#title_officer_error_msg').html('Something went wrong. Please try it again.').show();
+                $([document.documentElement, document.body]).animate({
+                    scrollTop: $("#title_officer_success_msg").offset().top
+                }, 1000);
+
+                setTimeout(function () {
+                    $('#title_officer_error_msg').html('').hide();
+                }, 4000);
+            }
+        });
+    });
+}
