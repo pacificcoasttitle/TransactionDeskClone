443784eec97c57aa121828ac2acfc13cc0f5b7f9
crestpiyush <piyush.j@crestinfosystems.net>
2024-11-28 17:55:55 +0530

    Stop partner data to ResWare from LP to ResWare order based on admin flag

--- application/modules/admin/controllers/order/Home.php
+++ application/modules/admin/controllers/order/Home.php
@@ -4474,7 +4474,6 @@ class Home extends MX_Controller
 
         $order_id = $this->input->post('order_id');
         $order_details = $this->order_model->get_order_details($order_id);
-
         $lpFileNumber = $order_details['lp_file_number'];
         $splitName = explode(' ', $order_details['primary_owner']);
         $ownerLastName = end($splitName);
@@ -4953,6 +4952,9 @@ class Home extends MX_Controller
                     }
                     }
                     <<<<<<< HEAD
+                    <<<<<<< HEAD
+                    =======
+                    >>>>>>> 6be259f8 (Stop partner data to ResWare from LP to ResWare order based on admin flag)
                     if (!empty($resPartners)) {
                     $key = array_search(7, array_column($resPartners['Partners'], 'PartnerTypeID'));
                     if (str_contains($resPartners['Partners'][$key]['PartnerName'], 'Doma Title Insurance') || $resPartners['Partners'][$key]['PartnerName'] == 'North American Title Insurance Company') {
@@ -5258,8 +5260,11 @@ class Home extends MX_Controller
                     }
                     }
                     }
+                    <<<<<<< HEAD
                     =======
                     >>>>>>> fc76d9cf (Create order softpro api and LP order changes)
+                    =======
+                    >>>>>>> 6be259f8 (Stop partner data to ResWare from LP to ResWare order based on admin flag)
                     }
 
                     if ($removePartnerFlag == 1 && isset($key) && strlen($key) > 0) {
