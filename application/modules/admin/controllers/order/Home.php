e511e5dac29838378ac167375d0e435c90849a8b
crestpiyush <piyush.j@crestinfosystems.net>
2024-11-28 17:55:55 +0530

    Stop partner data to ResWare from LP to ResWare order based on admin flag

--- application/modules/admin/controllers/order/Home.php
+++ application/modules/admin/controllers/order/Home.php
@@ -4474,7 +4474,6 @@ class Home extends MX_Controller
 
         $order_id = $this->input->post('order_id');
         $order_details = $this->order_model->get_order_details($order_id);
-
         $lpFileNumber = $order_details['lp_file_number'];
         $splitName = explode(' ', $order_details['primary_owner']);
         $ownerLastName = end($splitName);
