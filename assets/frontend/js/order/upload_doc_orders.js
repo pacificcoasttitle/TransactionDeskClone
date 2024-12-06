2dd85fb99529fe1ef2f030879b7052e581608d62
crestpiyush <piyush.j@crestinfosystems.net>
2024-12-06 15:43:09 +0530

    File Upload feature for User dashboard

--- assets/frontend/js/order/upload_doc_orders.js
+++ assets/frontend/js/order/upload_doc_orders.js
@@ -15,18 +15,18 @@ $(document).ready(function () {
             },
             /*"searching": false,*/
             initComplete: function () {
-                
-                
+
+
             },
             dom: 'Bfrtip',
             buttons: [],
             "drawCallback": function () {
-                
+
             },
             "ordering": false,
             "serverSide": true,
             "ajax": {
-                url: base_url + "get-orders-upload-doc", 
+                url: base_url + "get-orders-upload-doc",
                 type: "post",
                 error: function (XMLHttpRequest, textStatus, errorThrown) {
                     if (parseInt(XMLHttpRequest.status) == 419) {
@@ -40,6 +40,52 @@ $(document).ready(function () {
                     $("#upload_doc_orders tbody").append(
                         '<tr><td colspan="4" class="text-center">No records found</td></tr>');
                     $("#upload_doc_orders_processing").css("display", "none");
+
+                }
+            }
+        });
+    }
+
+    if ($('#uploaded_document_list').length) {
+        uploaded_document_list = $('#uploaded_document_list').DataTable({
+            // "pageLength": 2,
+            "paging": true,
+            "lengthChange": false,
+            "language": {
+                paginate: {
+                    next: '<span class="fa fa-angle-right"></span>',
+                    previous: '<span class="fa fa-angle-left"></span>',
+                },
+                "emptyTable": "Record(s) not found.",
+                "search": "",
+            },
+            /*"searching": false,*/
+            initComplete: function () {
+
+
+            },
+            dom: 'Bfrtip',
+            buttons: [],
+            "drawCallback": function () {
+
+            },
+            "ordering": false,
+            "serverSide": true,
+            "ajax": {
+                url: base_url + "get-uploaded-desk-doc",
+                type: "post",
+                error: function (XMLHttpRequest, textStatus, errorThrown) {
+                    if (parseInt(XMLHttpRequest.status) == 419) {
+                        alert("You are logged out. Please login.");
+                    }
+                    if (parseInt(XMLHttpRequest.status) == 419) {
+                        setTimeout(function () {
+                            location.reload();
+                        }, 1000);
+                    }
+                    $("#uploaded_document_list tbody").append(
+                        '<tr><td colspan="4" class="text-center">No records found</td></tr>');
+                    $("#uploaded_document_list_processing").css("display", "none");
 
                 }
             }
