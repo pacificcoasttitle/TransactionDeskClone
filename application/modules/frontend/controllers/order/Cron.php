42f0783692985048d58e9ff82ba1140b1ddc8553
crestpiyush <piyush.j@crestinfosystems.net>
2025-03-26 19:04:24 +0530

    Date range issue for fetching order fixed

--- application/modules/frontend/controllers/order/Cron.php
+++ application/modules/frontend/controllers/order/Cron.php
@@ -6963,12 +6963,14 @@ class Cron extends MX_Controller
         $titleOfficerList = array_column($query->result_array(), 'id', 'officer_name');
 
         // print_r($productTypeList);die;
-        $queryParams = "DateFrom=" . urlencode('03-01-2025') . "&DateTo=" . urlencode('03-31-2025');
+        $queryParams = "DateFrom=03-01-2025&DateTo=03-10-2025";
+        // $queryParams = "DateFrom=" . urlencode('03-01-2025') . "&DateTo=" . urlencode('03-31-2025');
         $reqData     = json_encode($req);
         $logid = $this->apiLogs->syncLogs($userdata['id'], 'softpro', 'get_softpro_orders', 'get_softpro_orders', $reqData, [], 0, 0);
         $response    = $this->softpro->make_request('GET', 'get_softpro_orders', $reqData, $queryParams);
         $this->apiLogs->syncLogs($userdata['id'], 'softpro', 'get_softpro_orders', 'get_softpro_orders', $reqData, json_encode($response), 0, $logid);
         // echo "<pre>";
+        // print_r($response);die;
         $sheetData = [];
         if ($response['status'] == 'success' && !empty($response['data'])) {
             
