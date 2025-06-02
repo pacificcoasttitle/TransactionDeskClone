0c5a492383395efae226d9e03521658ec203c9e4
crestpiyush <piyush.j@crestinfosystems.net>
2025-06-02 14:58:43 +0530

    Sales rep dashboard changes

--- assets/frontend/js/order/sales_dashboard.js
+++ assets/frontend/js/order/sales_dashboard.js
@@ -526,9 +526,9 @@ function getContacts(fileNumber) {
                             type = 'Underwriter';
                         }
                         if (key == 'escrow' || key == 'lender' || key == 'listing_agent') {
-                            table_data += '<tr><td>' + type + '</td><td>' + value.company_name + '</td><td>' + value.lookup_code + '</td><td>' + value.name + '</td><td>' + value.email_address + '</td></tr>';
+                            table_data += '<tr><td>' + type + '</td><td>' + value.company_name + '</td><td>' + value.name + '</td><td>' + value.email_address + '</td></tr>';
                         } else if (key == 'title_officer' || key == 'underwritter') {
-                            table_data += '<tr><td>' + type + '</td><td>' + value.company_name + '</td><td>' + value.lookup_code + '</td><td></td><td></td></tr>';
+                            table_data += '<tr><td>' + type + '</td><td>' + value.company_name + '</td><td></td><td></td></tr>';
                         }
                     });
                 }
