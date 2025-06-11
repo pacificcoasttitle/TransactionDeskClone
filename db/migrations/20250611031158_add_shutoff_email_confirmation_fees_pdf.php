c3beda3e5ba838a8c9126223fe956aac53826193
crestpiyush <piyush.j@crestinfosystems.net>
2025-06-11 11:37:58 +0530

    shutoff fees pdf in confirmation email

---
+++ db/migrations/20250611031158_add_shutoff_email_confirmation_fees_pdf.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types=1);
+
+use Phinx\Migration\AbstractMigration;
+
+final class AddShutoffEmailConfirmationFeesPdf extends AbstractMigration
+{
+    /**
+     * Change Method.
+     *
+     * Write your reversible migrations using this method.
+     *
+     * More information on writing migrations is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     *
+     * Remember to call "create()" or "update()" and NOT "save()" when working
+     * with the Table class.
+     */
+    public function change(): void
+    {
+        $table = $this->table('pct_configs');
+        $rows = [
+            [
+                'title' => 'Fees Pdf Confirmation Email Shut Off',
+                'slug' => 'fees_pdf_confirmation_email_shut_off',
+                'is_enable' => 0,
+            ]
+        ];
+
+        $table->insert($rows)->saveData();
+    }
+}
