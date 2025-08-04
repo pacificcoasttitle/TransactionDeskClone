7187faedac93e21075f859035fe0ef9d25ad0fc8
crestpiyush <piyush.j@crestinfosystems.net>
2025-08-04 16:07:53 +0530

    Recording email log issue fixed

--- application/modules/admin/views/order/home/recording_email_logs.php
+++ application/modules/admin/views/order/home/recording_email_logs.php
@@ -13,8 +13,8 @@
                         <tr>
                             <th width="5%">Sr No</th>
                             <th width="15%">Order Number</th>
-                            <th width="25%">Lender Name</th>
-                            <th width="40%">Company Name</th>
+                            <th width="25%">Receiving Partner</th>
+                            <th width="40%">Partner Company Name</th>
                             <th width="15%">Email Sent</th>
                             <!-- <th width="15%">Created</th> -->
                         </tr>
